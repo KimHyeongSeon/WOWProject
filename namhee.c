1efde344ea2c83c25ff3fe1d04e9026f2b58a90e
yonghyun3221 <yonghyun3221@gmail.com>
2016-09-20 13:45:24 +0900

    수정
    1번라인 필요없는 문장제거
    2번라인 헤더 include -> #include
    들여쓰기수정

--- namhee.c
+++ namhee.c
@@ -1,31 +1,30 @@
-Enter file contents here
-include<stdio.h> 
+
+#include<stdio.h> 
  void sort(int *a,int len) 
  { 
      int i,j,t;   
    
-    for( i = 0;i<len-1;++i) 
-    { 
+     for( i = 0;i<len-1;++i) 
+     { 
         for(j = i+1;j<len;++j)//或者 j = 0;j<len-i-1;++j 
         { 
             if(a[j] >a[j+1]) 
             { 
                 t  = a[j]; 
-                 a[j] = a[j+1]; 
+                a[j] = a[j+1]; 
                 a[j+1] = t; 
             } 
         } 
      } 
-          
  } 
  void main() 
  { 
      int a[6] = {10,2,8,-8,11,0}; 
-    int i = 0; 
+     int i = 0; 
      sort(a,6); 
       
      for(i = 0; i<6;++i) 
-    { 
+     { 
          printf("%d ",a[i]); 
      } 
      printf("\n"); 
