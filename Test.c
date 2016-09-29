a8e4228bf7183028d8b5442f5b02fb676b6f9f39
yonghyun3221 <yonghyun3221@gmail.com>
2016-09-29 10:42:24 +0900

    들여쓰기
    들여쓰기

--- Test.c
+++ Test.c
@@ -3,34 +3,30 @@ Enter file contents here
  
  
 int main() 
-{ 
+ {
+   int array[10] = {5,1,9,8,4,6,7,0,2,3};
+   int i, swapped; 
 
-int array[10] = {5,1,9,8,4,6,7,0,2,3};
-int i, swapped; 
-
-while (swapped > 0) 
-{ 
-swapped = 0; 
-
-for (i = 0; i < 9; i++)
-{
-
-   if (array[i] > array[i + 1]) 
-   { 
-   int temp = array[i]; 
-   array[i] = array[i + 1]; 
-   array[i + 1] = temp; 
-   swapped = 1;
-   } 
- } 
+   while (swapped > 0) 
+   {
+     swapped = 0; 
+     for (i = 0; i < 9; i++)
+     {
+       if (array[i] > array[i + 1]) 
+       {
+         int temp = array[i]; 
+         array[i] = array[i + 1]; 
+         array[i + 1] = temp; 
+         swapped = 1;
+       } 
+    } 
     for (i = 0; i < 10; i++) 
     { 
-     printf("%d  ", array[i]); 
+      printf("%d  ", array[i]); 
     } 
-    
-    printf("\n\n");    
-    } 
+     printf("\n\n");   
+  } 
 
- 
-  return 0; 
+
+   return 0; 
 } 
