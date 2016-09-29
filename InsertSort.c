20e03b5fa3d1e4c504c298356b1689bbf39d4974
yonghyun3221 <yonghyun3221@gmail.com>
2016-09-29 10:36:36 +0900

    전체적인 줄정리
    줄정리

--- InsertSort.c
+++ InsertSort.c
@@ -3,11 +3,23 @@ void sequential_insertion_sort(int *data, int n)
   int i, j, min, rem;
   for(i = 0; i < n-1; i++)
   {
-    for(j = i+1, min = i; j < n; j++) if (data[min] > data[j]) min = j;
-    if (min == i) continue;
+    for(j = i+1, min = i; j < n; j++)
+    {
+      if (data[min] > data[j])
+      {
+        min = j;
+      }
+    }
+    if (min == i)
+    {
+      continue;
+    }
 ​
     rem = data[min];
-    while(min > i) data[min] = data[--min];
+    while(min > i)
+    {
+      data[min] = data[--min];
+    }
     data[i] = rem;
   }
 }
