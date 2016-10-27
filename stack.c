a38c16f532a6d8848fbd31d69a1eda1b4072e1f1
yonghyun3221 <yonghyun3221@gmail.com>
2016-10-27 12:42:42 +0900

    들여쓰기 수정

--- stack.c
+++ stack.c
@@ -1,25 +1,26 @@
 #include "stdafx.h"
 #include "stdlib.h"
 #include "string.h"
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-char *array[4];
-int a;
-char stack=4;
+    char *array[4];
+    int a;
+    char stack=4;
 
-*array=&stack;
+    *array=&stack;
 
-*array=(char*)malloc(sizeof(char));
-stack=(char)malloc(stack*sizeof(char));
-array[0]="선문";
-array[1]="서울";
-array[2]="고려";
-array[3]="연세";
+    *array=(char*)malloc(sizeof(char));
+    stack=(char)malloc(stack*sizeof(char));
+    array[0]="선문";
+    array[1]="서울";
+    array[2]="고려";
+    array[3]="연세";
 
-for(a=0;a<4;a++)
-{
-printf("%s\n".array[a]);
-}
-system("pause");
-return 0;
+    for(a=0;a<4;a++)
+    {
+       printf("%s\n".array[a]);
+    }
+    system("pause");
+    return 0;
 }
