2ca911ff157e81751c21bae586d68771f06e868c
namheekim <skagmlrla0819@daum.net>
2016-11-08 13:40:19 +0900

    Create 소리.c
    출처 -http://egloos.zum.com/top2blue/v/5173007-

---
+++ 소리.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<Windows.h>
+#include<MMSystem.h>
+#pragma comment(lib,"Winmm.lib")
+int main(){	
+sndPlaySoundA("C:\\hello.wav",SND_ASYNC|SND_NODEFAULT);  	
+return 0;
+}
+
+//멜로디로 출력하려면
+#include<stdio.h>
+#include<Windows.h>
+int main(){
+Beep(523,100); //음,길이	Sleep(100); 
+//길이	
+// Beep의 앞은 523(도) 587(레) 659 698 784 880 988(시) 1046 (도)	
+return 0;
+}
