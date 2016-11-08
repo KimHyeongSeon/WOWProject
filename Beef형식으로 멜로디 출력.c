d161b69bed2bc37fc59f5c08d8b99955aee3f61b
namheekim <skagmlrla0819@daum.net>
2016-11-08 13:35:33 +0900

    Create Beef형식으로 멜로디 출력.c
    Beep(음의 주파수, 음표의 길이)
    음의 주파수 부분은 '옥타브 및 음계별 표준 주파수'를 참고하면 편하다.
    -음의 주파수 부분을 #define do 1046  식으로 정의해두면 편함
    - 쉼표의 경우 Sleep()함수를 사용하면 편함.
    
    http://cfocus.tistory.com/2 -출처-

---
+++ Beef형식으로 멜로디 출력.c
@@ -0,0 +1,29 @@
+//(1)헤더파일
+#include<windows.h>
+
+//(2)기본사용형식
+#include<stdio.h>
+#include<Windows.h> 
+#define _C 1046.502
+#define _D 1108.731
+#define _E 1318.510
+#define _F 1396.913
+#define _G 1567.982
+#define _A 1760.000
+#define _B 1975.533 
+void main(){ 
+Beep(_C, 100); 
+Sleep(200); 
+Beep(_D, 100); 
+Sleep(200); 
+Beep(_E, 100); 
+Sleep(200); 
+Beep(_F, 100); 
+Sleep(200); 
+Beep(_G, 100); 
+Sleep(200); 
+Beep(_A, 100); 
+Sleep(200); 
+Beep(_B, 100); 
+system("pause");
+}
