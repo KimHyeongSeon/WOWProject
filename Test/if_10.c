caa9f6f39ae27dd65c7e57b576220b18876f9fe0
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 01:03:36 +0900

    Create if_10.c

---
+++ Test/if_10.c
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char a;
+	
+	
+	printf("문자를 입력하시오 : ");
+	scanf("%c",&a);
+	
+	printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a,a);
+
+
+
+
+	
+	system("pause");
+	return 0;
+}
+
+
