050455b5a28974966c62e4080a42ca51256130f0
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 00:49:19 +0900

    Update and rename if_09.c to while_01.c

---
+++ Test/while_01.c
@@ -0,0 +1,21 @@
+ 
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a;
+
+	a=1;
+	while(a<16)
+	{
+		printf("%d ",a);
+		a++;
+	}
+
+	system("pause");
+	return 0;
+}
+
+
