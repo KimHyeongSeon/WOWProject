07ab8a9d9eeaf1f45c9fa37b58f39cab2c1baa3f
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 00:43:11 +0900

    create if_04.c
    create if_04.c

--- Test/if_04.c
+++ Test/if_04.c
@@ -1,1 +1,36 @@
  
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+ int weight;
+ 
+ printf("몸무게는??? ");
+ scanf("%d",&weight);
+
+ if(weight<=50.80)
+ {
+  printf("플라이급");
+ }
+ else if(weight<=61.23)
+ {
+  printf("라이트급");
+ }
+ else if(weight<=72.57)
+ {
+  printf("미들급");
+ }
+ else if(weight<=88.45)
+ {
+  printf("크루저급");
+ }
+ else
+ {
+  printf("헤비급");
+ }
+
+ 
+
+ system ("pause");
+ return 0;
+} 
