2bce0b54a76979cbc799a3f16cc50a11d350a513
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 00:41:25 +0900

    Update if_01.c

--- Test/if_01.c
+++ Test/if_01.c
@@ -1,1 +1,27 @@
  
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+ int a;
+
+ printf("정수를 입력하시오 : ");
+ scanf("%d",&a);
+
+ printf("%d\n",a);
+ 
+ if(a<0)
+ {
+  printf("음수입니다.");
+ }
+ 
+
+ 
+
+ 
+
+ system ("pause");
+ return 0;
+}
