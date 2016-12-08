f7de4b45e0caf04dfac0a7e6c4784901cbe4339b
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 00:41:53 +0900

    Update if_02.c

--- Test/if_02.c
+++ Test/if_02.c
@@ -1,1 +1,31 @@
  
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+ int tall;
+ int weight;
+ int fat;
+ 
+ printf("키는??? ");
+ scanf("%d",&tall);
+ printf("뭄무게는??? ");
+ scanf("%d",&weight);
+
+ fat=weight+100-tall;
+
+ if(0<fat)
+ {
+ printf("비만\n");
+ printf("%d",fat);
+ }
+
+ 
+
+
+ system ("pause");
+ return 0;
+}
