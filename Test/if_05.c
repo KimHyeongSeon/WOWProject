e6ba49fa29d2ce2b4dcdc7366d7f4a17ffc7ca75
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 00:45:26 +0900

    Update if_05.c

--- Test/if_05.c
+++ Test/if_05.c
@@ -1,1 +1,32 @@
  
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+ double x,y;
+
+ printf("정수1 ");
+ scanf("%lf",&x);
+ printf("정수2 ");
+ scanf("%lf",&y);
+
+ if(x,y>4.0)
+ {
+  printf("A");
+ }
+ else if(x,y>3.0)
+ {
+  printf("B");
+ }
+ else
+ {
+  printf("c");
+ }
+
+
+ system ("pause");
+ return 0;
+} 
