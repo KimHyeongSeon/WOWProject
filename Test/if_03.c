fb0221ca2e96db1e49eb46bf02a653dc7624b780
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 00:42:33 +0900

    create if_03.c
    create if_03.c

--- Test/if_03.c
+++ Test/if_03.c
@@ -1,1 +1,29 @@
  
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+ int age;
+
+ printf("현제나이는???");
+ scanf("%d",&age);
+
+ if(20<=age)
+ {
+  printf("성인입니다");
+ }
+ else
+ {
+  printf("당신은 %d년 후에 성인이 됩니다.",20-age);
+ }
+
+ 
+
+ 
+
+ system ("pause");
+ return 0;
+}
