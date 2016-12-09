fb9cb21d22406860a90ba0876e1e7def73cbddaa
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 14:10:54 +0900

    Update jungol_controlStatementH_04.c

--- Test/jungol_controlStatementH_04.c
+++ Test/jungol_controlStatementH_04.c
@@ -1,1 +1,25 @@
  
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int x;
+
+	printf("1. 개\n2. 고양이\n3. 병아리\n영어로 알고 싶은 번호를 입력하세요. ");
+	scanf("%d", &x);
+
+	switch (x)
+	{
+	case 1: printf("dog"); break;
+	case 2: printf("cat"); break;
+	case 3: printf("chick"); break;
+	default: printf("I don't know.");
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
