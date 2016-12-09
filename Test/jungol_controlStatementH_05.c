44747e911e1988fb18586c0b8cbfa53c09083b1c
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 14:11:30 +0900

    Update jungol_controlStatementH_05.c

--- Test/jungol_controlStatementH_05.c
+++ Test/jungol_controlStatementH_05.c
@@ -1,1 +1,30 @@
  
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int x;
+
+	scanf("%d", &x);
+
+	switch (x)
+	{
+	case 1: printf("31"); break;
+	case 2: printf("28"); break;
+	case 3: printf("31"); break;
+	case 4: printf("30"); break;
+	case 5: printf("31"); break;
+	case 6: printf("30"); break;
+	case 7: printf("31"); break;
+	case 8: printf("31"); break;
+	case 9: printf("30"); break;
+	case 10: printf("31"); break;
+	case 11: printf("30"); break;
+	case 12: printf("31"); break;
+	}
+	
+
+	system("pause");
+	return 0;
+}
