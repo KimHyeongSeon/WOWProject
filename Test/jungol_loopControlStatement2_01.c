6bf0fd20ba1204f56099829771822687c8360b93
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 14:29:08 +0900

    Create jungol_loopControlStatement2_01.c

---
+++ Test/jungol_loopControlStatement2_01.c
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	char alphabet;
+	int i;
+
+	scanf("%c", &alphabet);
+
+	for (i = 0; i < 20; i++)
+	{
+		printf("%c", alphabet);
+	}
+
+
+
+	system("pause");
+	return 0;
+}
