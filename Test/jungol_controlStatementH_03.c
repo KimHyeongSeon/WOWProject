c95e840f326b77eae994ba5a71e9b04d5b0942ab
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 14:10:39 +0900

    Update jungol_controlStatementH_03.c

--- Test/jungol_controlStatementH_03.c
+++ Test/jungol_controlStatementH_03.c
@@ -1,1 +1,35 @@
  
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int year;
+
+	scanf("%d", &year);
+
+	if (year % 4 == 0)
+	{
+		if (year % 4 == 0 && year % 100 == 0)
+		{
+			if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+			{
+				printf("윤년");
+				return 0;
+			}
+			printf("평년");
+			return 0;
+		}
+		printf("윤년");
+		return 0;
+	}
+	else
+	{
+		printf("평년");
+	}
+
+
+
+	system("pause");
+	return 0;
+}
