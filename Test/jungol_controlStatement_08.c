275929b6b3bf94d2bff565a9614cd7394233c676
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 13:53:15 +0900

    Create jungol_controlStatement_08.c

---
+++ Test/jungol_controlStatement_08.c
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	double grade;
+
+	scanf("%lf", &grade);
+
+	switch ((int)grade)
+	{
+	case 4: printf("""장학금을 받아가세요.""");
+		break;
+	case 3: printf("""다음 학기를 준비하세요.""");
+		break;
+	case 2: printf("""계절 학기에 등록하세요.""");
+		break;
+	default: printf("""다음 학기에 재수강하세요.""");
+	}
+
+
+	system("pause");
+	return 0;
+}
