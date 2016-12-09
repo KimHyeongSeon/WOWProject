7f237d54ca4acd48901fe0e2b9936dd4e2f1db09
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-09 14:26:46 +0900

    Create jungol_inputH_04.c

---
+++ Test/jungol_inputH_04.c
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int main()
+{
+	int x, y, z;
+
+	scanf("%d%d%d", &x, &y, &z);
+
+	printf("합 = %d\n", x + y + z);
+	printf("평균 = %d", (x + y + z) / 3);
+
+
+	system("pause");
+	return 0;
+}
+
