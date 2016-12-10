115dada112110afca87737c690cc05efff3448e7
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 22:33:33 +0900

    Update if_18.c

--- Test/if_18.c
+++ Test/if_18.c
@@ -6,10 +6,16 @@
 void main()
 {
 	int y;
+	
 	printf("년도를 입력하세요.\n");
 	scanf("%d", &y);
+	
 	if (y % 4 == 0)
+	{
 		printf("윤년입니다.\n");
+	}
 	else
+	{
 		printf("%d 년 후에 윤년입니다.\n", 4-(y % 4));
+	}
 }
