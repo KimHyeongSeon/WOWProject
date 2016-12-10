bf13a16b56ad07c993ab54d65f77ee0da2b2c1bb
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:31:05 +0900

    Create if04.c

---
+++ Test/if04.c
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+{
+
+	int g;
+	printf("당신의 성적을 입력하시오.\n");
+	scanf("%d", &g);
+	if (g < 0)
+		printf("잘못입력하셨습니다.\n");
+	else if (g < 50)
+		printf("F\n");
+	else if (g < 60)
+		printf("E\n");
+	else if (g < 70)
+		printf("D\n");
+	else if (g < 80)
+		printf("C\n");
+	else if (g < 90)
+		printf("B\n");
+	else
+		printf("A\n");
+
+}
