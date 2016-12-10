c5dc951dcc69527e80deff34bf55815674d200f9
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 23:20:42 +0900

    Update switch_03.c

--- Test/switch_03.c
+++ Test/switch_03.c
@@ -4,28 +4,31 @@
 
 
 void main()
-
 {
 	int key, i, j;
+	
+	
 	printf("높이를 입력하세요.\n");
-	scanf("%d", &i);
+	scanf("%d", &i);	
 	printf("밑변을 입력하세요.\n");
-	scanf("%d", &j);
+	scanf("%d", &j);	
 	printf("1.삼각형의 넓이 2.사각형넓이\n");
 	scanf("%d", &key);
+	
 	if (key != 1 && key != 2)
 	{
 		printf("잘못입력하셨습니다.\n");
 		return;
 	}
+	
 	switch (key)
 	{
-	case 1:
-		printf("삼각형의 넓이는 %d 입니다.", i*j / 2);
-		break;
-	case 2:
-		printf("사각형의 넓이는 %d 입니다.", i*j);
-		break;
+		case 1:
+			printf("삼각형의 넓이는 %d 입니다.", i*j / 2);
+			break;
+		case 2:
+			printf("사각형의 넓이는 %d 입니다.", i*j);
+			break;
 	}
 }
 
