2c677814addf0b4f8493960f8fe8667d088dee21
tkdfhr8744 <tkdfhr8744@naver.com>
2016-12-10 22:40:15 +0900

    Create point_01.c

---
+++ point_01.c
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "stdlib.h"
+int main(void)
+{
+  char *pc;
+  int *pi;
+  double *pd;
+  
+  printf("pc:%d\n", sizeof(pc));
+  printf("pi:%d\n", sizeof(pi));
+  printf("pd:%d\n", sizeof(pd)):
+  
+  printf("char* :%d\n", sizeof(char*));
+  printf("short*:%d\n", sizeof(short*));
+  printf("int*  :%d\n", sizeof(int*));
+  printf("float :%d\n", sizeof(float));
+  printf("double*:%d\n", sizeof(double));
+  
+  sustem ("pause");
+  return 0;
+ }
+  
