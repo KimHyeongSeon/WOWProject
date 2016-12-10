b10a9f33c67c06201495f8f683602f437a200296
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:07:27 +0900

    Create doWhile04.c

---
+++ Test/doWhile04.c
@@ -0,0 +1,20 @@
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+void main(void)
+{
+int i = 1, k, s;
+	printf("10층짜리 피라미드를 출력하는 프로그램입니다.\n");
+	do{
+		k = 1;
+		for (s = 10; s > i; s--)
+			printf(" ");
+		do{
+			printf("*");
+			k++;
+		} while (k < 2 * i);
+		printf("\n");
+		i++;
+	} while (i <= 10);
+}
