13e6acfd217c121ea30c36eb409e3a0765acf879
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:37:32 +0900

    Create if07.c

---
+++ Test/if07.c
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+{
+	int x, y;
+	printf("두 정수를 입력하세요.\n");
+	scanf("%d %d", &x, &y);
+	if (x >= y)
+		printf("두값의 차는 %d 입니다.\n", x - y);
+	else
+		printf("두값의 차는 %d 입니다.\n", y - x);
+}
