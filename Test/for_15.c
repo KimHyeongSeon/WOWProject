ea807d1d9663c18a0c03873eba817743f86989d9
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:58:07 +0900

    Update and rename for09.c to for_15.c

--- Test/for_15.c
+++ Test/for_15.c
@@ -1,21 +1,22 @@
 #include "stdafx.h"
 #include "stdlib.h"
 
-
-
-void main()
+int main()
 {
 	int i, j, result;
+	
 	printf("자연수를 입력하시오.\n");
 	scanf("%d", &j);
 	if (j <= 0)
 	{
-		printf("잘못입력하셨습니다.");
+		printf("잘못입력하셨습니다.\n");
 		return 0;
 	}
 	result = 1;
 	for (i = 1; i <= j; i++)
+	{
 		result = result*i;
+	}
 	printf("%d 팩토리얼은 %d 입니다.\n", j, result);
 	return 0;
 }
