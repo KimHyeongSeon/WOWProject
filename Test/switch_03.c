a3d5f6f20b54ca9b10219fcd86b2a7ddf29d763d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 23:11:21 +0900

    Create switch_03.c

---
+++ Test/switch_03.c
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+
+{
+	int key, i, j;
+	printf("높이를 입력하세요.\n");
+	scanf("%d", &i);
+	printf("밑변을 입력하세요.\n");
+	scanf("%d", &j);
+	printf("1.삼각형의 넓이 2.사각형넓이\n");
+	scanf("%d", &key);
+	if (key != 1 && key != 2)
+	{
+		printf("잘못입력하셨습니다.\n");
+		return;
+	}
+	switch (key)
+	{
+	case 1:
+		printf("삼각형의 넓이는 %d 입니다.", i*j / 2);
+		break;
+	case 2:
+		printf("사각형의 넓이는 %d 입니다.", i*j);
+		break;
+	}
+}
+
+
+
