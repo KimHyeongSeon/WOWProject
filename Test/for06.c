98e31361504fdffc0e336e660eca4c03723341e7
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:50:28 +0900

    Create for06.c

---
+++ Test/for06.c
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+{
+	int i, j;
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			printf("%d * %d = %2d\n", i, j, i*j);
+		}
+		printf("\n");
+	}
+}
