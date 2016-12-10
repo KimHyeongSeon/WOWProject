69d07ed6172c9bd0a4279e1ed0d7a902131771e6
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 01:58:07 +0900

    Create while02.c

---
+++ Test/while02.c
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+
+{
+	int h, p, i;
+	printf("높이를 입력하시오.\n");
+	scanf("%d", &h);
+	if (h <= 0)
+	{
+		printf("잘못입력하셨습니다.\n");
+		return;
+	}
+	p = h;
+	while (p > 0)
+	{
+		i = p;
+		while (i < h)
+		{
+			printf(" ");
+			i++;
+		}
+		i = 1;
+		while (i <= p)
+		{
+			printf("*");
+			i++;
+		}
+		printf("\n");
+		p--;
+	}
+}
