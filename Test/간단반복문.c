bcec2889efc5a991516a21e4a60c25f6eecfb209
namheekim <skagmlrla0819@daum.net>
2016-12-11 03:13:16 +0900

    Create 간단반복문.c

---
+++ Test/간단반복문.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+   int i,j;
+    for(i=0; i<9; i++)
+    {
+       if(i<5)
+       {
+          for(j+0; j<5-i; j++)
+          {
+            printf("*");
+          }
+       }
+     }
+       else
+       {
+         for(j+0;j<=4;j++)
+         {
+            printf("*");
+         }
+         printf("\n");
+       }
+       return 0;
+    }
+    
+  
