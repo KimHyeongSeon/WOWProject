4a7ed25c5bc2c7afe8b583c6e17ffe1fa9131600
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:11:24 +0900

    Create doWhile08.c

---
+++ Test/doWhile08.c
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+void main()
+{
+
+  int i, k;
+	printf("높이를 입력하세요.\n");
+	scanf("%d", &i);
+	
+	do
+  {
+		k = 1;
+		do
+    {
+			printf("*");
+			k++;
+		}
+    while (k <= i);
+		i--;
+		
+		printf("\n");
+	} 
+  while (i > 0);
+    
+}
