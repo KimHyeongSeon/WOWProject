7ee5e2bef2dd426dbf584d956ebd11be58d0620d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 22:20:11 +0900

    Update and rename for08.c to for_18.c

--- Test/for_18.c
+++ Test/for_18.c
@@ -6,8 +6,10 @@
 void main()
 {
 	int i, j, sum;
+	
 	printf("자연수 2개를 입력하시오.\n");
 	scanf("%d %d", &i, &j);
+	
 	if (i <= 0 || j <= 0)
 	{
 		printf("잘못입력하셨습니다.\n");
@@ -21,7 +23,10 @@ void main()
 	}
 	sum = 0;
 	for (; i <= j; i++)
+	{
 		sum += i;
+	}
+	
 	printf("두 수와 두 수 사이에 존재하는 수들의 합은 %d 입니다.\n", sum);
 
 }
