517bd571f77d4590596b6987273282d5be9e5e67
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:14:17 +0900

    Create doWhile10.c

---
+++ Test/doWhile10.c
@@ -0,0 +1,26 @@
+#include "stdlib.h"
+#include "stdafx.h"
+
+void main(void)
+{
+	int i, j = 2, k, f=0;
+	printf("1보다 큰 자연수를 입력하세요.\n");
+	scanf("%d", &i);
+	printf("자연수이하의 소수는 ");
+	do{
+		f = 0;
+		k = 2;
+		do{
+			if (j%k == 0)
+			{
+				f++;
+				break;
+			}
+			k++;
+		} while (k<j);
+		if (f == 0)
+			printf("%d ", j);
+		j++;
+	} while (j<i);
+	printf("입니다.\n");
+}
