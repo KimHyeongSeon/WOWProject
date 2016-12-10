b0bf19a1bb863a755c63713deb4abd041ee18798
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 22:27:51 +0900

    Update if_13.c

--- Test/if_13.c
+++ Test/if_13.c
@@ -5,13 +5,17 @@
 
 void main()
 {
-
-	int a;
-	printf("당신의 나이를 입력하세요.\n");
+	int a;	
+	
+	printf("당신의 나이를 입력하세요.\n");	
 	scanf("%d", &a);
+	
 	if (a >= 20)
+	{
 		printf("성인입니다.\n");
+	}
 	else
+	{
 		printf("당신은 %d년 후에 성인이 됩니다.\n", 20 - a);
-
+	}
 }
