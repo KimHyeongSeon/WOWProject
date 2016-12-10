006550ca277ce9f66c0c57363cf84722ddf07b3f
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 23:16:16 +0900

    Update and rename switch09.c to switch_09.c

--- Test/switch_09.c
+++ Test/switch_09.c
@@ -4,25 +4,30 @@
 
 
 void main()
-
 {
 	int key, i, h, k, s;
+	
 	printf("높이를 입력하세요.\n");
 	scanf("%d", &h);
+	
 	printf("1.하노이타워 2.피라미드\n");
 	scanf("%d", &key);
+	
 	if (key != 1 && key != 2)
 	{
 		printf("잘못입력하셨습니다.\n");
 		return;
 	}
-	switch (key)
+	
+	switch (key)		
 	{
 	case 1:
 		for (i = 1; i <= h; i++)
 		{
 			for (k = 1; k <= i; k++)
+			{
 				printf("*");
+			}
 			printf("\n");	
 		}
 		break;
@@ -30,9 +35,13 @@ void main()
 		for (i = 1; i <= h; i++)
 		{
 			for (s = h; s > i; s--)
+			{
 				printf(" ");
+			}
 			for (k = 1; k <= 2 * i - 1; k++)
+			{
 				printf("*");
+			}
 			printf("\n");
 		}
 	
