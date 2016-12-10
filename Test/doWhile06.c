740273be8d3767c9bf03baa71745779efcf7e6d3
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:09:30 +0900

    Create doWhile06.c

---
+++ Test/doWhile06.c
@@ -0,0 +1,17 @@
+
+#include "stdlib.h"
+#include "stdafx.h"
+
+void doWhile5(void)
+{
+  int i = 1, j = 1;
+	do{
+		j = 1;
+		do{
+			printf("%d * %d = %d\n", i, j, i*j);
+			j++;
+		} while (j <= 9);
+		printf("\n");
+		i++;
+	} while (i <= 9);
+}
