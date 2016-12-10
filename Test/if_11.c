4b22df5bbd3e699df41c606bc3f4e70718b7955d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 22:26:34 +0900

    Update and rename if01.c to if_11.c

--- Test/if_11.c
+++ Test/if_11.c
@@ -12,13 +12,15 @@ void main()
 	printf("정수를 입력하세요\n");
 	scanf("%d", &i);
 	if (i > 0)
+	{
 		printf("양의 정수입니다.\n");
+	}
 	else if (i < 0)
+	{
 		printf("음의 정수입니다.\n");
+	}
 	else
+	{
 		printf("0입니다.\n");
-
-
-
-
+	}
 }
