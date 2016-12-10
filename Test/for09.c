c788cb57d70de2ddf59369fec6900d56234c2fa2
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:52:09 +0900

    Create for09.c

---
+++ Test/for09.c
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+{
+	int i, j, result;
+	printf("자연수를 입력하시오.\n");
+	scanf("%d", &j);
+	if (j <= 0)
+	{
+		printf("잘못입력하셨습니다.");
+		return 0;
+	}
+	result = 1;
+	for (i = 1; i <= j; i++)
+		result = result*i;
+	printf("%d 팩토리얼은 %d 입니다.\n", j, result);
+	return 0;
+}
