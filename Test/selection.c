7cd6a87181ed7a46897150d46be9a6da6eaff7a3
tkdfhr8744 <tkdfhr8744@naver.com>
2016-12-10 23:05:52 +0900

    Create selection.c

---
+++ Test/selection.c
@@ -0,0 +1,29 @@
+#include "Stdio.h"
+#include "stdlib.h"
+
+int main()
+{
+  int a[7],i,j,temp;
+  for(i=0;i<7;i++)
+  {
+  scanf("%d",a[i]);
+  }
+  for(i=0;i<6;i++)
+  {
+    for(j=1+1;j<7;j++)
+    {
+      if(a[i]>a[j])
+      {
+        tmp=a[i];
+        a[i]=a[j];
+        a[j]=tmp;
+       }
+     }
+    }
+    for(i=0;i<7;i++)
+    {
+      printf("%d",a[i]);
+     }
+     system("pause");
+     return 0;
+  }
