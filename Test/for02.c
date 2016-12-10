1c99f2dba1ce4bb74a1a096281e65cb0e97bc9ac
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:48:11 +0900

    Create for02.c

---
+++ Test/for02.c
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+
+void main()
+{
+	int h, p, i;
+	printf("높이를 입력하세요.\n");
+	scanf("%d", &h);
+	if (h <= 0)
+	{
+		printf("잘못입력하셨습니다.\n");
+		return;
+	}
+	for (p = 1; p <= h; p++)
+	{
+		for (i = h; i > p; i--)
+			printf(" ");
+		for (i = 1; i <= p; i++)
+			printf("*");
+		printf("\n");
+	}
+}
