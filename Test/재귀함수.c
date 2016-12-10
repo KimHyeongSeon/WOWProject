7f3f774de358086bf9f6064f7677fa98051909b5
namheekim <skagmlrla0819@daum.net>
2016-12-11 02:57:42 +0900

    Create 재귀함수.c

---
+++ Test/재귀함수.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+void Recursive( int nCount )
+{
+if( nCount <= 0 )   
+{
+printf( "발사\n" );     
+return ;
+}
+else
+{ 
+printf( "카운트 다운 : %d\n", nCount ); 
+
+Recursive( nCount-1 );  
+//인자로 받은 nCount에 -1을 줄여서 호출한다.    
+}
+}
+int main( )
+{
+Recursive( 5 );
+return 0;
+}
+
