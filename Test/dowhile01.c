6583c45eefc21e4501eab3ea290e6b947929eab4
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-10 21:03:19 +0900

    Create dowhile01.c

---
+++ Test/dowhile01.c
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+void main()
+{
+	int i=1;
+	printf("1~100을 출력하는 프로그램입니다.\n");
+	do
+  {
+		printf("%d ", i);
+		i++;
+	} 
+  while (i<=100);
+  
+  
+	printf("\n");  
+}
