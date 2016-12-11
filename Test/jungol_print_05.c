0f76e4e8479762b88754ffae45d594fb25aa38dd
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:53:52 +0900

    Create jungol_print_05.c

---
+++ Test/jungol_print_05.c
@@ -0,0 +1,30 @@
+// jungol.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//출력 - 형성평가 5
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int main()
+{
+	printf("%15s", "Seoul");
+	printf("%15s", "10,312,545");
+	printf("%15s", "+91,375\n");
+	printf("%15s", "Pusan");
+	printf("%15s", "3,567,910");
+	printf("%15s", "+5,868\n");
+	printf("%15s", "Incheon");
+	printf("%15s", "2,758,296");
+	printf("%15s", "+64,888\n");
+	printf("%15s", "Daegu");
+	printf("%15s", "2,511,676");
+	printf("%15s", "+17,230\n");
+	printf("%15s", "Gwangju");
+	printf("%15s", "1,454,636");
+	printf("%15s", "+29,744\n");
+
+
+	system ("pause");
+    return 0;
+}
+
