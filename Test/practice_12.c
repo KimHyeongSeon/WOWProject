8cd6acee2ab40c0249c1e318f9416c7f485d88d3
let0000 <wlsgh8309@naver.com>
2016-12-12 01:42:49 +0900

    Update practice_12.c

--- Test/practice_12.c
+++ Test/practice_12.c
@@ -1,1 +1,9 @@
-#include <stdio.h>                int main(){ int a[5]; int *p = a; int i; scanf_s("%d %d %d %d %d", (p + 0), (p + 1), (p + 2), (p + 3), (p + 4));  for (i = 0; i < 5; i+=2)  printf("%d ", p[i]); return 0;}
+#include <stdio.h>    
+
+int main()
+{
+  int a[5];
+  int *p = a;
+  int i; 
+  
+  scanf_s("%d %d %d %d %d", (p + 0), (p + 1), (p + 2), (p + 3), (p + 4));  for (i = 0; i < 5; i+=2)  printf("%d ", p[i]); return 0;}
