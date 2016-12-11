42fc61b22eb85c41b22bd35c7eb9fa5f961fb512
let0000 <wlsgh8309@naver.com>
2016-12-12 00:38:57 +0900

    Update pointer_39.c

--- Test/pointer_39.c
+++ Test/pointer_39.c
@@ -1,1 +1,5 @@
-#include<stdio.h>int main(void){ int a = 35; int b = 60; int *ptr1 = &a; int *ptr2 = &b; printf("ptr1+ptr2=%d\n", *ptr1 + *ptr2); return 0;}
+#include<stdio.h>
+
+int main(void)
+{
+  int a = 35; int b = 60; int *ptr1 = &a; int *ptr2 = &b; printf("ptr1+ptr2=%d\n", *ptr1 + *ptr2); return 0;}
