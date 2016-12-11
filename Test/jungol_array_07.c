0f3e085e3af75302278872d52a58778ca4419df6
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:36:16 +0900

    Update jungol_array_07.c

--- Test/jungol_array_07.c
+++ Test/jungol_array_07.c
@@ -1,1 +1,49 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int hundredUpMin = 10000;
+	int hundredDownMax = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (j = 0; j < 10; j++)
+	{
+		if (arr[j] > 100 && arr[j] < hundredUpMin)
+		{
+			hundredUpMin = arr[j];
+		}
+	}
+
+	for (k = 0; k < 10; k++)
+	{
+		if (arr[k] < 100 && hundredDownMax < arr[k])
+		{
+			hundredDownMax = arr[k];
+		}
+	}
+
+	if (hundredDownMax < 1)
+	{
+		hundredDownMax = 100;
+	}
+	if (hundredUpMin > 9999)
+	{
+		hundredUpMin = 100;
+	}
+	
+
+	printf("%d %d", hundredDownMax, hundredUpMin);
+
+	system("pause");
+	return 0;
+}
