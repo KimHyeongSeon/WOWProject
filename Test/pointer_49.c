d03245865979b418925c83a528e513ddf4747dbe
let0000 <wlsgh8309@naver.com>
2016-12-12 01:06:03 +0900

    Update pointer_49.c

--- Test/pointer_49.c
+++ Test/pointer_49.c
@@ -1,1 +1,5 @@
-#include <stdio.h>void swap(int * q,int * p){    int temp = *p;    *p = *q;    *q = temp;}int main(){    int a = 10, b = 2, x = 3, y = 5;    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);    swap(&x, &y);    swap(&a, &b);    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);} 
+#include <stdio.h>
+
+void swap(int * q,int * p)
+{   
+  int temp = *p;    *p = *q;    *q = temp;}int main(){    int a = 10, b = 2, x = 3, y = 5;    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);    swap(&x, &y);    swap(&a, &b);    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);} 
