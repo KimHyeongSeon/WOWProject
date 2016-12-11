982b65e0c7c6416136e92e32b9e564e79c791079
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:11:26 +0900

    Create function3_04.c

---
+++ Test/function3_04.c
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "stdlib.h"
+#include "math.h"
+
+int dice(int a)
+{
+
+
+}
+
+int main()
+{
+	int N = 0;
+
+	scanf("%d", &N);
+	
+	dice(N);
+
+
+	system("pause");
+	return 0;
+}
