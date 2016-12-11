54d86874830e0050dc0e1a61ed7cfdd9bd673757
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:36:28 +0900

    Create orangeActivity.java

---
+++ Test/orangeActivity.java
@@ -0,0 +1,17 @@
+package npp.a1234567;
+
+/**
+ * Created by user on 2016-12-02.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class orangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orange);
+
+    }
+}
