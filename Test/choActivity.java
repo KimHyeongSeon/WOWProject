7dcc8d91d0a58982a7045834a7863bcd90053eb9
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:42:58 +0900

    Update choActivity.java

--- Test/choActivity.java
+++ Test/choActivity.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class choActivity extends AppCompatActivity {
-
+public class choActivity extends AppCompatActivity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cho);
 
