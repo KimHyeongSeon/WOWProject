206cd23c3a65fbd377f8882d436cbd96e32d62dc
let0000 <wlsgh8309@naver.com>
2016-12-12 01:01:09 +0900

    Update pointer_45.c

--- Test/pointer_45.c
+++ Test/pointer_45.c
@@ -5,4 +5,7 @@ int main(int argc, const char * argv[])
   int *p;
   int *q;  
   
-  p = (int *) 0x1004;    q = (int *) 0x1000;    printf("p : %x\nq : %x\n",p,q);    printf("p-q : %x\n",p-q);    printf("(unsigned int)p+(unsigned int)q : %x\n",(unsigned int)p +(unsigned int)q);    return 0;}
+  p = (int *) 0x1004;  
+  q = (int *) 0x1000;   
+  
+  printf("p : %x\nq : %x\n",p,q);    printf("p-q : %x\n",p-q);    printf("(unsigned int)p+(unsigned int)q : %x\n",(unsigned int)p +(unsigned int)q);    return 0;}
