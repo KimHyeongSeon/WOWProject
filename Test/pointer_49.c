8122d6f44a876476a875fba2e66c67fb7d215ff7
let0000 <wlsgh8309@naver.com>
2016-12-12 01:06:25 +0900

    Update pointer_49.c

--- Test/pointer_49.c
+++ Test/pointer_49.c
@@ -7,4 +7,8 @@ void swap(int * q,int * p)
   *q = temp;
 }
 
-int main(){    int a = 10, b = 2, x = 3, y = 5;    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);    swap(&x, &y);    swap(&a, &b);    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);} 
+int main()
+{  
+  int a = 10, b = 2, x = 3, y = 5;  
+  
+  printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);    swap(&x, &y);    swap(&a, &b);    printf("a,b,x,y: %d,%d,%d,%d\n", a, b, x, y);} 
