6de8ad26df4ada093385f7a9ee72d4408d2fc15d
let0000 <wlsgh8309@naver.com>
2016-12-12 00:32:41 +0900

    Update pointer_34.c

--- Test/pointer_34.c
+++ Test/pointer_34.c
@@ -1,1 +1,3 @@
-#include<stdio.h>int main(void){ int a = 50; int* b; b = &a; *b += 60; printf("%d\n", a); return 0;}
+#include<stdio.h>
+
+int main(void){ int a = 50; int* b; b = &a; *b += 60; printf("%d\n", a); return 0;}
