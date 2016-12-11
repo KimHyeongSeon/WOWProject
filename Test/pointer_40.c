fed98de2f13be480f5b42a7ee786451eab9d54c9
let0000 <wlsgh8309@naver.com>
2016-12-12 00:40:15 +0900

    Update pointer_40.c

--- Test/pointer_40.c
+++ Test/pointer_40.c
@@ -2,4 +2,8 @@
 
 int main(void)
 {
-  int a = 1; int* b; b = &a; printf("a의 값은 %d 입니다.\n", a); printf("a의 주소값은 %p입니다.\n", &a); printf("포인터형 변수 b의 주소값은 %p입니다.\n", b); return 0;}
+  int a = 1;
+  int* b;
+  b = &a;
+  
+  printf("a의 값은 %d 입니다.\n", a); printf("a의 주소값은 %p입니다.\n", &a); printf("포인터형 변수 b의 주소값은 %p입니다.\n", b); return 0;}
