a5032d2ff56d59805b02611f6e985269d333c3bd
let0000 <wlsgh8309@naver.com>
2016-12-12 02:10:47 +0900

    Update practice_28.c

--- Test/practice_28.c
+++ Test/practice_28.c
@@ -1,1 +1,20 @@
-#include <stdio.h>int main() { double num[5]; int i; double *p; p = &num;  for (i = 0; i <= 4; i++) {   scanf("%lf", &num[i]);  } for (i = 0; i <= 4; i++) {  printf("%.1lf ", p[i]); } return 0;}
+#include <stdio.h>
+
+int main() 
+{
+  double num[5]; 
+  int i; 
+  double *p;
+  p = &num;
+  
+  for (i = 0; i <= 4; i++)
+  {
+    scanf("%lf", &num[i]);
+  }
+  
+  for (i = 0; i <= 4; i++)
+  {
+    printf("%.1lf ", p[i]); 
+  }
+  
+  return 0;}
