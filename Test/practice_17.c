daa4be459962060dbdea9b80cf777596861b8e6e
let0000 <wlsgh8309@naver.com>
2016-12-12 01:50:02 +0900

    Update practice_17.c

--- Test/practice_17.c
+++ Test/practice_17.c
@@ -2,4 +2,9 @@
 
 int main(void)
 {
-  int foo[] = { 519, 31, 7988, 165326, 10, -24642, 134 };  printf("%d\n", sizeof(foo));   printf("%d\n", sizeof(foo) / sizeof(foo[0]));   return 0;}
+  int foo[] = { 519, 31, 7988, 165326, 10, -24642, 134 }; 
+  
+  printf("%d\n", sizeof(foo)); 
+  printf("%d\n", sizeof(foo) / sizeof(foo[0]));  
+  
+  return 0;}
