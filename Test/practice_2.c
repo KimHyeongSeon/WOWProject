4bfcad2b5f0ecb1b3ba543b203b8c27fedd651c7
let0000 <wlsgh8309@naver.com>
2016-12-12 01:19:04 +0900

    Update practice_2.c

--- Test/practice_2.c
+++ Test/practice_2.c
@@ -1,1 +1,5 @@
-#include <stdio.h>int main(){    int array[3] = {45, 67, 89};    printf ("%p\n", array);    printf ("%p\n", & array);    printf ("%p\n", & array[0]);    return 0;}
+#include <stdio.h>
+
+int main()
+{   
+  int array[3] = {45, 67, 89};    printf ("%p\n", array);    printf ("%p\n", & array);    printf ("%p\n", & array[0]);    return 0;}
