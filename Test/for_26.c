d33a4432062959c1af6bbfacdb0f1f3822c56280
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:32:10 +0900

    Create for_26.c

---
+++ Test/for_26.c
@@ -0,0 +1,21 @@
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i, j;
+
+	for (i = 1; i < 10; i++)
+	{
+		j = 5;
+		printf("%d * %d = %d\n", j, i, j*i);
+	}
+
+
+
+	system("pause");
+	return 0;
+}
+
