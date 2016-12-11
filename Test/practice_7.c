b47e914c8b39338633db11e6765e97b532411a07
let0000 <wlsgh8309@naver.com>
2016-12-12 01:26:30 +0900

    Update practice_7.c

--- Test/practice_7.c
+++ Test/practice_7.c
@@ -8,4 +8,9 @@ void main()
   int a[SIZE] = { 200, 300, 400, 500, 600 };
   salary(&*a, SIZE);  
   
-  printf("월급의 합%d \n", salary(&*a,SIZE));}int salary(int *A,int size){ int i,sum=0; for (i = 0; i < SIZE; i++)  sum += A[i]; return sum;}
+  printf("월급의 합%d \n", salary(&*a,SIZE));
+}
+
+int salary(int *A,int size)
+{ 
+  int i,sum=0; for (i = 0; i < SIZE; i++)  sum += A[i]; return sum;}
