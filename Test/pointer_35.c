c0baa5b7d04565675723c501e3ca93f8f75631e2
let0000 <wlsgh8309@naver.com>
2016-12-12 00:34:10 +0900

    Update pointer_35.c

--- Test/pointer_35.c
+++ Test/pointer_35.c
@@ -2,4 +2,8 @@
 
 int main(void)
 {
-  int arr[7] = { 101, 202, 303, 404, 505, 606, 707 }; int(*p)[7] = &arr; int i; for (i = 0; i < 7; i++)  printf("%d ", (*p)[i]); printf("\n"); return 0;}
+  int arr[7] = { 101, 202, 303, 404, 505, 606, 707 };
+  int(*p)[7] = &arr;
+  int i;
+  
+  for (i = 0; i < 7; i++)  printf("%d ", (*p)[i]); printf("\n"); return 0;}
