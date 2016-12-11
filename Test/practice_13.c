4b344c1e0e33df62186026ff14da1c7cdc6be773
let0000 <wlsgh8309@naver.com>
2016-12-12 01:44:01 +0900

    Update practice_13.c

--- Test/practice_13.c
+++ Test/practice_13.c
@@ -6,4 +6,9 @@ int main()
   double *p = a;
   int i; 
   
-  scanf_s("%lf %lf %lf %lf %lf", (p + 0), (p + 1), (p + 2), (p + 3), (p + 4)); for (i = 0; i < 5; i++)  printf("%.1lf ", p[i]); return 0;}
+  scanf_s("%lf %lf %lf %lf %lf", (p + 0), (p + 1), (p + 2), (p + 3), (p + 4));
+  
+  for (i = 0; i < 5; i++) 
+    printf("%.1lf ", p[i]);
+  
+  return 0;}
