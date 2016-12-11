93d7a2e727c872a4806ca50bb8bbe0a32733a99b
let0000 <wlsgh8309@naver.com>
2016-12-12 00:09:14 +0900

    Update pointer_22.c

--- Test/pointer_22.c
+++ Test/pointer_22.c
@@ -1,1 +1,13 @@
-#include <stdio.h>                int main(){ int arr[5] = {1, 2, 3, 4, 5}; int *p = &arr[0]; int i; for (i = 0; i < 5; i++, p++)  printf("%d\n", *p);    return 0;}
+#include <stdio.h>   
+
+int main()
+{ 
+  int arr[5] = {1, 2, 3, 4, 5};
+  int *p = &arr[0];
+  int i;
+  
+  for (i = 0; i < 5; i++, p++)
+    printf("%d\n", *p);
+  
+  return 0;
+}
