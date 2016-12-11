736edaa8288e84e35dc8b4a194fd933050f0a7dd
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:33:52 +0900

    Update jungol_array_04.c

--- Test/jungol_array_04.c
+++ Test/jungol_array_04.c
@@ -1,1 +1,30 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int arr[100] = {};
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 100; i++)
+	{
+		scanf("%d", &arr[i]);
+
+		if (arr[i] == 0)
+		{
+			for (j = i - 1; j >= 0; j--)
+			{
+				printf("%d ", arr[j]);				
+			}
+			break;
+		}
+	}
+
+
+
+
+	system("pause");
+	return 0;
+}
