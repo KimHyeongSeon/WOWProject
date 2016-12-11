5fcf7327b6faebdcb0410110286ea7213581eac0
let0000 <wlsgh8309@naver.com>
2016-12-12 00:30:13 +0900

    Update pointer_32.c

--- Test/pointer_32.c
+++ Test/pointer_32.c
@@ -1,2 +1,8 @@
 #include <stdio.h>
-int main(void){ char *pc; int *pi; double *pd; printf("pc의 크기 : %d\n", sizeof(pc)); printf("pi의 크기 : %d\n", sizeof(pi)); printf("pd의 크기 : %d\n", sizeof(pd)); printf("char* 의 크기 : %d\n", sizeof(char*)); printf("short* 의 크기 : %d\n", sizeof(short*)); printf("int* 의 크기 : %d\n", sizeof(int*)); printf("float* 의 크기 : %d\n", sizeof(float*)); printf("double*의 크기 : %d\n", sizeof(double*)); return 0;}
+int main(void)
+{ 
+  char *pc;
+  int *pi;
+  double *pd;
+  
+  printf("pc의 크기 : %d\n", sizeof(pc)); printf("pi의 크기 : %d\n", sizeof(pi)); printf("pd의 크기 : %d\n", sizeof(pd)); printf("char* 의 크기 : %d\n", sizeof(char*)); printf("short* 의 크기 : %d\n", sizeof(short*)); printf("int* 의 크기 : %d\n", sizeof(int*)); printf("float* 의 크기 : %d\n", sizeof(float*)); printf("double*의 크기 : %d\n", sizeof(double*)); return 0;}
