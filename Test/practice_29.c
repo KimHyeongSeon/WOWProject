4629bfd04dc82c0915d381ff342649ebc7e4785d
let0000 <wlsgh8309@naver.com>
2016-12-12 02:12:17 +0900

    Update practice_29.c

--- Test/practice_29.c
+++ Test/practice_29.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 
-int main() { double num[100] = { 0 }, sum = 0, average = 0; int i, length; double *p; p = &num;  scanf_s("%d", &length); for (i = 0; i <= length - 1; i++) {  scanf_s("%lf", &p[i]);  sum += num[i]; } average = sum / length; for (i = 0; i <= length - 1; i++) {  printf("%.2lf ", p[i]); } puts(""); printf("합계 : %.2lf\n평균 : %.2lf\n", sum, average); return 0;}
+int main()
+{
+  double num[100] = { 0 }, sum = 0, average = 0;
+  int i, length; 
+  double *p; p = &num;  
+  
+  scanf_s("%d", &length);
+  
+  for (i = 0; i <= length - 1; i++)
+  {
+    scanf_s("%lf", &p[i]);
+    sum += num[i]; 
+  }
+  
+  average = sum / length;
+  
+  for (i = 0; i <= length - 1; i++)
+  {
+    printf("%.2lf ", p[i]);
+  }
+  
+  puts(""); 
+  printf("합계 : %.2lf\n평균 : %.2lf\n", sum, average); 
+  
+  return 0;}
