6b89d7017e0fe0a3245a7fafd85bbef6fdd73780
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:30:55 +0900

    Create stActivity.java

---
+++ Test/stActivity.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * Created by user on 2016-12-02.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class stActivity extends AppCompatActivity 
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_st);
+
+    }
+
+}
