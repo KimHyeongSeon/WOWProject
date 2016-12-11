d75e75ae8e0fde7fee5e514a4fe716fedd8bb3d6
let0000 <wlsgh8309@naver.com>
2016-12-12 01:30:41 +0900

    Update practice_10.c

--- Test/practice_10.c
+++ Test/practice_10.c
@@ -2,4 +2,11 @@
 
 int main()
 { 
-  int num; int *p; p = &num; scanf_s("%d", p); printf("%d...%d", *p / 10, *p % 10); return 0;}
+  int num;
+  int *p;
+  p = &num;
+  
+  scanf_s("%d", p);
+  printf("%d...%d", *p / 10, *p % 10);
+  
+  return 0;}
