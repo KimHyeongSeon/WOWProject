8bc1cdf6fb99540d3645aca2ab94b19a36e1f55a
let0000 <wlsgh8309@naver.com>
2016-12-12 00:56:19 +0900

    Update and rename pointer_42 to pointer_42.c

---
+++ Test/pointer_42.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) 
+{  
+  { 
+    char ch;        char *p;                p = &ch;                printf("address ch = %p\n", &ch);        printf("address p = %p\n",p);            }        return 0;}
