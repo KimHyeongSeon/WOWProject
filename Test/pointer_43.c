5bc2dbe8e530dc6540080db8d0806124ad36b7e6
let0000 <wlsgh8309@naver.com>
2016-12-12 00:58:09 +0900

    Update pointer_43.c

--- Test/pointer_43.c
+++ Test/pointer_43.c
@@ -9,4 +9,10 @@ int main(int argc, char*argv[])
   ch1 ='a';  
   ch2 ='b';  
   
-  printf("ch1 = %c\n", ch1);    printf("ch2 = %c\n", ch2);    p = &ch1;    printf("*p1 = %c\n", *p);    p = &ch2;    printf("*p2 = %c\n", *p);    return 0;}
+  printf("ch1 = %c\n", ch1);  
+  printf("ch2 = %c\n", ch2);  
+  
+  p = &ch1; 
+  printf("*p1 = %c\n", *p);  
+  p = &ch2; 
+  printf("*p2 = %c\n", *p);    return 0;}
