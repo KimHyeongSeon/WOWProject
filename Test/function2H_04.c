1faf2973aee50fda7b507f36b64ab44407efae70
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:09:57 +0900

    Create function2H_04.c

---
+++ Test/function2H_04.c
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "stdlib.h"
+#include "math.h"
+
+
+int main()
+{
+	int num = 0;
+
+	scanf("%d", &num);
+
+	num = pow(2, num);
+
+	printf("%d", num);
+
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
