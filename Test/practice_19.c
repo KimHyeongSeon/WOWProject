a562bd55cd5fb9c21191e29921b328f4e6f0d296
let0000 <wlsgh8309@naver.com>
2016-12-12 01:53:21 +0900

    Update practice_19.c

--- Test/practice_19.c
+++ Test/practice_19.c
@@ -8,4 +8,9 @@ void InputArr(int arr[], int n)
     scanf("%d", (arr+i));
 }
 
-int GetMax(int arr[], int n){            int i;            int maxValue = arr[0];       for(i = 1; i < n; i++) {            if(maxValue < *(arr+i))        maxValue = *(arr+i); } return maxValue;}
+int GetMax(int arr[], int n)
+{
+  int i;  
+  int maxValue = arr[0];
+  
+  for(i = 1; i < n; i++) {            if(maxValue < *(arr+i))        maxValue = *(arr+i); } return maxValue;}
