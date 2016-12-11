84674ec96097c40e3bf468d69c70aba08f980140
let0000 <wlsgh8309@naver.com>
2016-12-12 00:59:51 +0900

    Update pointer_44.c

--- Test/pointer_44.c
+++ Test/pointer_44.c
@@ -14,4 +14,10 @@ void test2(char *p)
 
 int main(int argc, char*argv[])
 {   
-  char ch1 = 'z';    char *p;        p = &ch1;        printf("*p = %c\n",*p);        test1(p);    printf("ch1 = %c\n",ch1);    test2(p);    printf("ch1 = %c\n",ch1);    }
+  char ch1 = 'z'; 
+  *p;    
+  p = &ch1; 
+  
+  printf("*p = %c\n",*p);   
+  
+  test1(p);    printf("ch1 = %c\n",ch1);    test2(p);    printf("ch1 = %c\n",ch1);    }
