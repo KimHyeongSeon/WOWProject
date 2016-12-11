7f7baf2db39814b5692447ce0469ccf3d0894d6d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:59:06 +0900

    Update functhion_01.c

--- Test/functhion_01.c
+++ Test/functhion_01.c
@@ -1,1 +1,30 @@
 
+// functhion.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+void letter()
+{
+	printf("~!@#$^&*()_+|\n");
+}
+
+int main()
+{
+	int i = 0;
+	int num = 0;;
+
+	scanf("%d", &num);
+
+	for (i = 0; i < num; i++) 
+	{
+		letter();
+	}
+
+
+
+	system("pause");
+    return 0;
+}
+
