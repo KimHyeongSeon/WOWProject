56a40490dd5d922031a7e7e5b132807086dc1238
let0000 <wlsgh8309@naver.com>
2016-12-12 00:10:12 +0900

    Update pointer_23.c

--- Test/pointer_23.c
+++ Test/pointer_23.c
@@ -1,1 +1,22 @@
-#include <stdio.h>                int main(){ int arr[5] = { 1,2,3,4,5 }; int *p = &arr[0]; int i; for (i = 0; i < 5; i++) {  printf("&arr[%d] = %p, ", i, &arr[i]);  printf("arr + %d = %p\n", i, arr + i); } for (i = 0; i < 5; i++) {  printf("arr[%d] = %d, ", i, arr[i]);  printf("*(arr+%d) = %d\n", i, *(arr + i)); } return 0;}
+#include <stdio.h>          
+
+int main()
+{ 
+  int arr[5] = { 1,2,3,4,5 };
+  int *p = &arr[0];
+  int i;
+  
+  for (i = 0; i < 5; i++) 
+  {  
+    printf("&arr[%d] = %p, ", i, &arr[i]);
+    printf("arr + %d = %p\n", i, arr + i);
+  }
+  
+  for (i = 0; i < 5; i++)
+  {  
+    printf("arr[%d] = %d, ", i, arr[i]);
+    printf("*(arr+%d) = %d\n", i, *(arr + i));
+  }
+  
+  return 0;
+}
