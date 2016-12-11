bc40743b5e2d923ca1d0e7ede849b11f63c2fce8
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:05:22 +0900

    Create FinalProject_Lotto.java

---
+++ Test/FinalProject_Lotto.java
@@ -0,0 +1,76 @@
+package kr.ac.sunmoon;
+
+import java.util.Comparator;
+
+public class Lotto 
+{
+	private int turn;
+	private String date;
+	private String number1;
+	private String number2;
+	private String number3;
+	private String number4;
+	private String number5;
+	private String number6;
+	private String numberB;
+	
+	
+	public int getTurn() {
+		return turn;
+	}
+	public void setTurn(int turn) {
+		this.turn = turn;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getNumber1() {
+		return number1;
+	}
+	public void setNumber1(String number1) {
+		this.number1 = number1;
+	}
+	public String getNumber2() {
+		return number2;
+	}
+	public void setNumber2(String number2) {
+		this.number2 = number2;
+	}
+	public String getNumber3() {
+		return number3;
+	}
+	public void setNumber3(String number3) {
+		this.number3 = number3;
+	}
+	public String getNumber4() {
+		return number4;
+	}
+	public void setNumber4(String number4) {
+		this.number4 = number4;
+	}
+	public String getNumber5() {
+		return number5;
+	}
+	public void setNumber5(String number5) {
+		this.number5 = number5;
+	}
+	public String getNumber6() {
+		return number6;
+	}
+	public void setNumber6(String number6) {
+		this.number6 = number6;
+	}
+	public String getNumberB() {
+		return numberB;
+	}
+	public void setNumberB(String numberB) {
+		this.numberB = numberB;
+	}
+	
+	
+	
+
+}
