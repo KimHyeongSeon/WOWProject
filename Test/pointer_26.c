1523c8ed95337c0e455f9df570b81cb711f72372
let0000 <wlsgh8309@naver.com>
2016-12-12 00:12:25 +0900

    Update pointer_26.c

--- Test/pointer_26.c
+++ Test/pointer_26.c
@@ -1,1 +1,18 @@
-#include "stdio.h"int main(void){ float  a[5], number1; float *p =0; for (int i = 0; i < 5; i++) {  scanf("%f\n", &a[i]); } p = &a[0];  printf("%.1f %.1f %.1f %.1f %.1f", *p,*(p+1), *(p + 2) , *(p + 3),*(p + 4));}
+#include "stdio.h"
+
+int main(void)
+{ 
+  float  a[5], number1;
+  float *p =0;
+  
+  for (int i = 0; i < 5; i++)
+  { 
+    scanf("%f\n", &a[i]);
+  } 
+  
+  p = &a[0];
+  
+  printf("%.1f %.1f %.1f %.1f %.1f", *p,*(p+1), *(p + 2) , *(p + 3),*(p + 4));
+  
+  return 0;
+}
