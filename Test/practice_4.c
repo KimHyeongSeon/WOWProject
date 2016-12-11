2bd886b6cd440800d2e22244bd7af6bb05d08c2c
let0000 <wlsgh8309@naver.com>
2016-12-12 01:21:15 +0900

    Update practice_4.c

--- Test/practice_4.c
+++ Test/practice_4.c
@@ -1,1 +1,5 @@
-#include<stdio.h>int main(void){ int num1 = 10, num2 = 20; int *pnum; pnum = &num1; (*pnum) += 20; pnum = &num2; (*pnum) -= 20; printf("num1:%d, num2:%d \n", num1, num2); return 0;}
+#include<stdio.h>
+
+int main(void)
+{ 
+  int num1 = 10, num2 = 20; int *pnum; pnum = &num1; (*pnum) += 20; pnum = &num2; (*pnum) -= 20; printf("num1:%d, num2:%d \n", num1, num2); return 0;}
