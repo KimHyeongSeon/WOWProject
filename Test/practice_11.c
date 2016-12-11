d50aeeb5db69365667a9ae6bf4659c532ed077db
let0000 <wlsgh8309@naver.com>
2016-12-12 01:41:58 +0900

    Update practice_11.c

--- Test/practice_11.c
+++ Test/practice_11.c
@@ -2,4 +2,10 @@
 
 int main()
 {
-  int num1, num2; int *p1, *p2; p1 = &num1; p2 = &num2; scanf_s("%d %d", p1, p2); printf("%d + %d = %d\n", *p1, *p2, *p1 + *p2); printf("%d - %d = %d\n", *p1, *p2, *p1 - *p2); printf("%d * %d = %d\n", *p1, *p2, *p1 * *p2); printf("%d / %d = %d\n", *p1, *p2, *p1 / *p2); return 0;}
+  int num1, num2;
+  int *p1, *p2;
+  p1 = &num1;
+  p2 = &num2;
+  
+  scanf_s("%d %d", p1, p2);
+  printf("%d + %d = %d\n", *p1, *p2, *p1 + *p2); printf("%d - %d = %d\n", *p1, *p2, *p1 - *p2); printf("%d * %d = %d\n", *p1, *p2, *p1 * *p2); printf("%d / %d = %d\n", *p1, *p2, *p1 / *p2); return 0;}
