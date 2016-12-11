3d0e1ce3b83f16b0ebedab74d468c845dc44a1c4
let0000 <wlsgh8309@naver.com>
2016-12-12 00:19:00 +0900

    Update pointer_30.c

--- Test/pointer_30.c
+++ Test/pointer_30.c
@@ -1,2 +1,3 @@
 #include "stdio.h"
+
 void getsum(int x, int y, int *p1,int *p2);void main(){     int a,b;printf("a,b를 입력");scnaf("%d%d",&a,&b);getsum(a,b,&a,&b);printf("두정수의 합 :%d\n",a);printf("두정수의 차 :%d\n",b);}void getsum(int x, int y, int *p1,int *p2){*p1=x+y;if(x>=y)*p2=x-y;else*p2=y-x;}
