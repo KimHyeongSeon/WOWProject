1f63b745f09d59a691880371f53c4e78d8b60fce
let0000 <wlsgh8309@naver.com>
2016-12-12 02:02:58 +0900

    Update practice_22.c

--- Test/practice_22.c
+++ Test/practice_22.c
@@ -1,1 +1,11 @@
-#include <stdio.h>int main(void){ char c[4] = {'A', '\0', '\0', '\0'}; int *pi = (int *)&c[0]; printf("%d %c\n", (int)c[0], c[0]); printf("%d %c\n", *pi, (char) *pi); return 0;}
+#include <stdio.h>
+int main(void)
+{
+  char c[4] = {'A', '\0', '\0', '\0'};
+  int *pi = (int *)&c[0];
+  
+  printf("%d %c\n", (int)c[0], c[0]);
+  printf("%d %c\n", *pi, (char) *pi);
+  
+  return 0;
+}
