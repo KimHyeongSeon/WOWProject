05ae6bf8089ee11a4ff15fc3ecf12b86b245a52c
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:30:57 +0900

    Update jungol_array_2.c

--- Test/jungol_array_2.c
+++ Test/jungol_array_2.c
@@ -1,1 +1,24 @@
 
+// jungol_array.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int i = 0;
+
+
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	system("pause");
+    return 0;
+}
+
