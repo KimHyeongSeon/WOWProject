bd6443f19549395a5963ef95e0d91580ed42164f
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:31:41 +0900

    Update jungol_array_3.c

--- Test/jungol_array_3.c
+++ Test/jungol_array_3.c
@@ -1,1 +1,21 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int i = 0;
+	char arr[10] = {};
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf(" %c", &arr[i]);
+	}
+
+	printf("%c %c %c", arr[0], arr[3], arr[6]);
+
+
+
+	system("pause");
+	return 0;
+}
