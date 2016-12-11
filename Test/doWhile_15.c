24594d9c43d12ad842628ea128449333aca5fda4
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:28:35 +0900

    Create doWhile_15.c

---
+++ Test/doWhile_15.c
@@ -0,0 +1,22 @@
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+void GetSumProduct(int a, int b, int sum, int product);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i = 3;
+
+	do
+	{
+		printf("무조건 한번은 실행 좋건이 거짓이여도 ");
+
+	} while (i > 5);
+
+
+
+
+	system("pause");
+	return 0;
+}
