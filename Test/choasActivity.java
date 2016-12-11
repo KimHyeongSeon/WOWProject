70f4bb655238527059144fa69a3919a5cfa74377
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:43:19 +0900

    Update choasActivity.java

--- Test/choasActivity.java
+++ Test/choasActivity.java
@@ -1,18 +1,15 @@
-package npp.a1234567;
-
-/**
- * Created by user on 2016-12-02.
- */
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class choasActivity extends AppCompatActivity {
+public class choasActivity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choas);
 
