fb783216502c8765aacc74fdc94cfa3f19625883
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:15:44 +0900

    Update and rename doWhile03.c to doWhile_03.c

--- Test/doWhile_03.c
+++ Test/doWhile_03.c
@@ -6,11 +6,11 @@ void main(void)
 {
 	int i = 1, result = 1;
 	do
-  {
+	{
 		result = result * i;
 		i++;
 	} 
-  while (i <= 10);
+  	while (i <= 10);
   
 	printf("1~10까지의 곱은 %d 입니다.\n", result);
 }
