d36e0114d5f33c3bdd4814ec9877c6bdad741cab
let0000 <wlsgh8309@naver.com>
2016-12-12 00:54:41 +0900

    Update pointer_41.c

--- Test/pointer_41.c
+++ Test/pointer_41.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[])
-{         
-  char ch;     
-  char *p;    
-  p = &ch;       
+{ 
+  {
+    char ch;     
+    char *p;    
+    p = &ch;       
   
-  printf("address ch = %p\n", &ch);        printf("address p = %p\n",p);            }        return 0;}
+    printf("address ch = %p\n", &ch);   
+    printf("address p = %p\n",p);  
+  }        return 0;}
