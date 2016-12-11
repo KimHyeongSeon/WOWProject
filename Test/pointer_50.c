1357c3c57828260fc2ca8b09aae2ac266c6f003c
let0000 <wlsgh8309@naver.com>
2016-12-12 01:07:47 +0900

    Update pointer_50.c

--- Test/pointer_50.c
+++ Test/pointer_50.c
@@ -4,4 +4,7 @@ void rectangle(int a, int b, int * area, int * perim);
 
 int main()
 {
-  int x, y;    int area, perim;    printf("2개의 값을 입력하세요 띄어쓰기하여서 " );    scanf("%d %d", &x, &y);    rectangle(x, y, &area, &perim);    printf("넓이는 %d 둘레는 %d\n", area, perim);    return 0;}void rectangle(int a,int b,int * area,int * perim){    *area = a * b;    *perim = 2 * (a + b);}
+  int x, y;   
+  int area, perim; 
+  
+  printf("2개의 값을 입력하세요 띄어쓰기하여서 " );    scanf("%d %d", &x, &y);    rectangle(x, y, &area, &perim);    printf("넓이는 %d 둘레는 %d\n", area, perim);    return 0;}void rectangle(int a,int b,int * area,int * perim){    *area = a * b;    *perim = 2 * (a + b);}
