361d6a1aeaed51aa1f43f46423694b090eb73f9c
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:34:39 +0900

    Update jungol_array_05.c

--- Test/jungol_array_05.c
+++ Test/jungol_array_05.c
@@ -1,1 +1,16 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	double arr[6] = { 85.6, 79.5, 83.1, 80.0, 78.2, 75.0 };
+	int classA, classB;
+
+	scanf("%d%d", &classA, &classB);
+
+	printf("%.1lf", arr[classA - 1] + arr[classB - 1]);
+
+	system("pause");
+	return 0;
+}
