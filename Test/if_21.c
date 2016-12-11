f8a62f38e40f2d5946b95f21653a434d5b9fd030
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:41:31 +0900

    Update if_21.c

--- Test/if_21.c
+++ Test/if_21.c
@@ -6,13 +6,16 @@ int _tmain(int argc, _TCHAR* argv[])
 	int year;
 
 	printf("당신은 몇 년생입니까? : ");
-
 	scanf_s("%d", &year);                   //성인판별 프로그램
 
 	if (year>1995)
+	{	
 		printf("당신은 만19세 미만 미성인입니다\n");
+	}
 	else
+	{
 		printf("당신은 만19세 이상 성인입니다.\n");
+	}
 	
 	
 	system("pause");
