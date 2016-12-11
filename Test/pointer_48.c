ce068bad3c7ca0e4c101c73c69e598ce4f74ccc3
let0000 <wlsgh8309@naver.com>
2016-12-12 01:04:55 +0900

    Update pointer_48.c

--- Test/pointer_48.c
+++ Test/pointer_48.c
@@ -2,4 +2,8 @@
 
 int main()
 { 
-  int var =10;    int *p;    p= &var;        printf ( "\n Address of var is: %u", &var);    printf ( "\n Address of var is: %u", p);        printf ( "\n Address of pointer p is: %u", &p);        printf( "\n Value of pointer p is: %u", p);        printf ( "\n Value of var is: %d", var);    printf ( "\n Value of var is: %d", *p);    printf ( "\n Value of var is: %d", *( &var));}
+  int var =10;  
+  int *p;   
+  p= &var;   
+  
+  printf ( "\n Address of var is: %u", &var);    printf ( "\n Address of var is: %u", p);        printf ( "\n Address of pointer p is: %u", &p);        printf( "\n Value of pointer p is: %u", p);        printf ( "\n Value of var is: %d", var);    printf ( "\n Value of var is: %d", *p);    printf ( "\n Value of var is: %d", *( &var));}
