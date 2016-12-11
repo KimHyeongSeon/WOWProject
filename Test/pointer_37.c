e1155b25536e977556ed204954d7b7808559dc1f
let0000 <wlsgh8309@naver.com>
2016-12-12 00:36:26 +0900

    Update pointer_37.c

--- Test/pointer_37.c
+++ Test/pointer_37.c
@@ -1,1 +1,4 @@
-#include<stdio.h>int main(void){ int num = 10; int*ptr1 = &num; int*ptr2 = ptr1; (*ptr1)++; (*ptr2)++; printf("%d \n", num); return 0;}
+#include<stdio.h>
+
+int main(void)
+{ int num = 10; int*ptr1 = &num; int*ptr2 = ptr1; (*ptr1)++; (*ptr2)++; printf("%d \n", num); return 0;}
