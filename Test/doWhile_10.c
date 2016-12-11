ddb594f8e2b7b5652e70b5167d1382d2c66b0ef6
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:21:48 +0900

    Update and rename doWhile10.c to doWhile_10.c

--- Test/doWhile_10.c
+++ Test/doWhile_10.c
@@ -7,20 +7,28 @@ void main(void)
 	printf("1보다 큰 자연수를 입력하세요.\n");
 	scanf("%d", &i);
 	printf("자연수이하의 소수는 ");
-	do{
+	do
+	{
 		f = 0;
 		k = 2;
-		do{
+		do
+		{
 			if (j%k == 0)
 			{
 				f++;
 				break;
 			}
 			k++;
-		} while (k<j);
+		}
+		while (k<j);
+		
 		if (f == 0)
+		{
 			printf("%d ", j);
+		}
 		j++;
-	} while (j<i);
+	}
+	while (j<i);
+	
 	printf("입니다.\n");
 }
