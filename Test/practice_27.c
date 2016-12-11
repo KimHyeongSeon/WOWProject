128195181db6de7e9d4959ec34aaa6e06ed91ed0
let0000 <wlsgh8309@naver.com>
2016-12-12 02:09:25 +0900

    Update practice_27.c

--- Test/practice_27.c
+++ Test/practice_27.c
@@ -1,1 +1,19 @@
-#include <stdio.h>int main() { int num[5], i; int *p; p = &num;  for (i = 0; i <= 4; i++) {   scanf("%d", &num[i]);  } for (i = 0; i <= 4; i += 2) {  printf("%d ", p[i]); } return 0;}
+#include <stdio.h>
+
+int main()
+{
+  int num[5], i; 
+  int *p;
+  p = &num; 
+  
+  for (i = 0; i <= 4; i++)
+  {
+    scanf("%d", &num[i]);  
+  }
+  
+  for (i = 0; i <= 4; i += 2) 
+  { 
+    printf("%d ", p[i]);
+  } 
+  
+  return 0;}
