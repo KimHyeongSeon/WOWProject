31ce09db33a4641b367b8194d2d1437119ecd18c
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:00:21 +0900

    Create functhion_05.c

---
+++ Test/functhion_05.c
@@ -0,0 +1,36 @@
+ #include "stdafx.h"
+#include "stdlib.h"
+
+int pow(int x, int y)
+{
+	int i = 0;
+	int avr = 1;
+
+	for (i = 0; i < y; i++)
+	{
+		avr *= x;
+	}
+
+	printf("%d", avr);
+
+	return 0;
+}
+
+
+int main()
+{
+	int num = 0;
+	int square = 0;
+
+	scanf("%d", &num);
+	scanf("%d", &square);
+
+	pow(num, square);
+
+
+
+
+
+	system("pause");
+	return 0;
+}
