0d64a223696dd960078cd7f0e4600ebfc9c21451
let0000 <wlsgh8309@naver.com>
2016-12-12 01:47:58 +0900

    Update practice_15.c

--- Test/practice_15.c
+++ Test/practice_15.c
@@ -10,4 +10,10 @@ void PrintPoint(POINT *p);
 
 int main(void)
 {
-  POINT pt1 = { 100, 200 }; PrintPoint(&pt1); return 0;}void PrintPoint(POINT *p){ printf("(%d, %d)\n", p->x, p->y);}
+  POINT pt1 = { 100, 200 };
+  PrintPoint(&pt1);
+  
+  return 0;
+}
+
+void PrintPoint(POINT *p){ printf("(%d, %d)\n", p->x, p->y);}
