528cdb00a4c0107ad4a4b05ef2aeb3f237406a17
let0000 <wlsgh8309@naver.com>
2016-12-12 00:31:25 +0900

    Update pointer_33.c

--- Test/pointer_33.c
+++ Test/pointer_33.c
@@ -1,1 +1,3 @@
-#include <stdio.h>int main(void){ int arr[5] = { 10, 20, 30, 40, 50 }; int *p = &arr[0]; int i; for (i = 0; i < 5; i++, p++)  printf("%d\n", *p); return 0;}
+#include <stdio.h>
+
+int main(void){ int arr[5] = { 10, 20, 30, 40, 50 }; int *p = &arr[0]; int i; for (i = 0; i < 5; i++, p++)  printf("%d\n", *p); return 0;}
