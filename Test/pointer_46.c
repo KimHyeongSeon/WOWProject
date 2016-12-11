eb58f86bf8f4c1392974f55268fd610ef8dcd898
let0000 <wlsgh8309@naver.com>
2016-12-12 01:02:57 +0900

    Update pointer_46.c

--- Test/pointer_46.c
+++ Test/pointer_46.c
@@ -2,4 +2,14 @@
 
 void main()
 {   
-  int ary[3] = {10,100,1000};    int *pAry;    pAry = ary;    //pAry = &ary; // compile err    ??printf("ary : %d\n&ary : %d\npAry : %d\n",ary,&ary,pAry);    ? printf("Sizeof(ary) : %d\nSizeof(&ary) : %d\n", sizeof(ary),sizeof(&ary));    ??printf("*(ary+1) : %d\n*(&ary+1) : %d\n",*(ary+1),*(&ary+1));}
+  int ary[3] = {10,100,1000};  
+  int *pAry;  
+  pAry = ary;  
+  //pAry = &ary; //
+  
+  printf("ary : %d\n&ary : %d\npAry : %d\n",ary,&ary,pAry); 
+  printf("Sizeof(ary) : %d\nSizeof(&ary) : %d\n", sizeof(ary),sizeof(&ary));  
+  printf("*(ary+1) : %d\n*(&ary+1) : %d\n",*(ary+1),*(&ary+1));
+  
+  return 0;
+}
