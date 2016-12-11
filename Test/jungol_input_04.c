266ca74510ed377ad343d926d7b9a1c2d92c8564
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:52:55 +0900

    Create jungol_input_04.c

---
+++ Test/jungol_input_04.c
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int weight = 49;
+	double gravity = 0.2683;
+
+	printf("%d * %lf = %lf\n", weight, gravity, weight * gravity);
+
+
+	system("pause");
+	return 0;
+}
