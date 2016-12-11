fe7d454f4ce9e9994f68ebcfdb273a8120256b42
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:54:50 +0900

    Create 20161013_Student.java

---
+++ Test/20161013_Student.java
@@ -0,0 +1,26 @@
+package kr.ac.sunmoon;
+
+public class Student extends Person
+{
+	private String studentId;
+	
+	public Student()
+	{
+		System.out.println("student");
+	}
+	
+	
+	
+
+	public String getStudentId()
+	{
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) 
+	{
+		this.studentId = studentId;
+	}
+
+
+}
