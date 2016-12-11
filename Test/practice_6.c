53fa7ab66c9942c3aff957af04cea6f7cbdc4157
let0000 <wlsgh8309@naver.com>
2016-12-12 01:24:21 +0900

    Update practice_6.c

--- Test/practice_6.c
+++ Test/practice_6.c
@@ -1,1 +1,8 @@
-#include "stdio.h"#define SIZE 5void salary(const int *A, const int *B,int *C,int size);void main(){ int a[SIZE] = { 200, 300, 400, 500, 600 }; int b[SIZE] = { 30,60,90,120,150 }; int c[SIZE]; int j; salary(&*a, &*b, &*c, SIZE); printf("월급의 총액\n"); for (j = 0; j < SIZE; j++) {  printf("직원 %d %3d\n", j + 1, c[j]); }}void salary(const int *A, const int *B, int *C, int size){ int i; for (i = 0; i < SIZE; i++)  C[i] = A[i] + B[i];}
+#include "stdio.h"
+#define SIZE 5
+
+void salary(const int *A, const int *B,int *C,int size);
+
+void main()
+{
+  int a[SIZE] = { 200, 300, 400, 500, 600 }; int b[SIZE] = { 30,60,90,120,150 }; int c[SIZE]; int j; salary(&*a, &*b, &*c, SIZE); printf("월급의 총액\n"); for (j = 0; j < SIZE; j++) {  printf("직원 %d %3d\n", j + 1, c[j]); }}void salary(const int *A, const int *B, int *C, int size){ int i; for (i = 0; i < SIZE; i++)  C[i] = A[i] + B[i];}
