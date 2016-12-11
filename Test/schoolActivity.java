dd1aa3446e4a999c55749e2d28c8a4e0a2edd4b9
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:51:32 +0900

    Update schoolActivity.java

--- Test/schoolActivity.java
+++ Test/schoolActivity.java
@@ -1,14 +1,14 @@
-package npp.a1234567;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class schoolActivity extends AppCompatActivity {
+public class schoolActivity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school);
 
