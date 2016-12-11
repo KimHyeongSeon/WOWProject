7ae72ecc283948e2ca2c44a8d337975693019002
let0000 <wlsgh8309@naver.com>
2016-12-12 01:03:46 +0900

    Update pointer_47.c

--- Test/pointer_47.c
+++ Test/pointer_47.c
@@ -1,1 +1,4 @@
-#include <stdio.h>int main(void){    int array[3] = {10,20,30};        printf("%x %x %x \n", array,array+0, &array[0]);    printf("   %x %x\n", array+1,&array[1]);    printf("   %x %x\n", array+2,&array[2]);    printf("%d %d %d \n", sizeof(array),sizeof(array+0),sizeof(&array[0]));    return 0;}
+#include <stdio.h>
+int main(void)
+{ 
+  int array[3] = {10,20,30};        printf("%x %x %x \n", array,array+0, &array[0]);    printf("   %x %x\n", array+1,&array[1]);    printf("   %x %x\n", array+2,&array[2]);    printf("%d %d %d \n", sizeof(array),sizeof(array+0),sizeof(&array[0]));    return 0;}
