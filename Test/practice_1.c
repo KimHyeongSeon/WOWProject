ed5b79e9008ce24ec89a51c0a5858e57c206d495
let0000 <wlsgh8309@naver.com>
2016-12-12 01:17:21 +0900

    Update practice_1.c

--- Test/practice_1.c
+++ Test/practice_1.c
@@ -17,4 +17,8 @@ int main()
 
 void get_array(int a[], int size)
 {  
-  int i;    printf("10개의 값을 입력하세요, 한개 입력할때마다 엔터\n " );    for (i = 0; i < size; i++)        scanf("%d", &a[i]);}void prt_array(int a[], int size){    int i;    printf("모든값들 :\n");    for (i = 0; i < size; i++)        printf("%d\n", a[i]);}
+  int i; 
+  
+  printf("10개의 값을 입력하세요, 한개 입력할때마다 엔터\n " ); 
+  
+  for (i = 0; i < size; i++)        scanf("%d", &a[i]);}void prt_array(int a[], int size){    int i;    printf("모든값들 :\n");    for (i = 0; i < size; i++)        printf("%d\n", a[i]);}
