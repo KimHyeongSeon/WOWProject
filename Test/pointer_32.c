48b022d56461819fea1d8063b611eb1c1c4de4b7
let0000 <wlsgh8309@naver.com>
2016-12-12 00:30:40 +0900

    Update pointer_32.c

--- Test/pointer_32.c
+++ Test/pointer_32.c
@@ -10,4 +10,9 @@ int main(void)
   printf("pd의 크기 : %d\n", sizeof(pd));
   
   printf("char* 의 크기 : %d\n", sizeof(char*));
-  printf("short* 의 크기 : %d\n", sizeof(short*)); printf("int* 의 크기 : %d\n", sizeof(int*)); printf("float* 의 크기 : %d\n", sizeof(float*)); printf("double*의 크기 : %d\n", sizeof(double*)); return 0;}
+  printf("short* 의 크기 : %d\n", sizeof(short*));
+  printf("int* 의 크기 : %d\n", sizeof(int*));
+  printf("float* 의 크기 : %d\n", sizeof(float*));
+  printf("double*의 크기 : %d\n", sizeof(double*));
+  
+  return 0;}
