746cba32b7a9daab0a07d38686f2d94f1d48fd88
let0000 <wlsgh8309@naver.com>
2016-12-12 02:03:47 +0900

    Update practice_23.c

--- Test/practice_23.c
+++ Test/practice_23.c
@@ -1,1 +1,15 @@
-#include <stdio.h>int main(void){ int i = 20; int *pi = &i; int **dpi = &pi; printf("%p %p %p\n", &i, pi, *dpi); *pi = i + 2; printf("%d %d %d\n", i, *pi, **dpi); **dpi = *pi + 2; printf("%d %d %d\n", i, *pi, **dpi); return 0;}
+#include <stdio.h>
+
+int main(void)
+{
+  int i = 20; 
+  int *pi = &i;
+  int **dpi = &pi;
+  
+  printf("%p %p %p\n", &i, pi, *dpi);
+  *pi = i + 2;
+  printf("%d %d %d\n", i, *pi, **dpi);
+  **dpi = *pi + 2;
+  printf("%d %d %d\n", i, *pi, **dpi);
+  
+  return 0;}
