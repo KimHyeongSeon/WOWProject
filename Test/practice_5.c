7a858dff0c7c9c5c42e6bc3897af8752331de56a
let0000 <wlsgh8309@naver.com>
2016-12-12 01:22:52 +0900

    Update practice_5.c

--- Test/practice_5.c
+++ Test/practice_5.c
@@ -1,1 +1,5 @@
-#include<stdio.h>int main(void){ int i, j; int *pi; i = 10; j = i; j = j + 1; printf("i=%d,j=%d\n", i, j); i = 5; pi = &i; *pi = *pi + 1; printf("i=%d,*pi=%d\n", i, *pi); return 0;}
+#include<stdio.h>
+
+int main(void)
+{
+  int i, j; int *pi; i = 10; j = i; j = j + 1; printf("i=%d,j=%d\n", i, j); i = 5; pi = &i; *pi = *pi + 1; printf("i=%d,*pi=%d\n", i, *pi); return 0;}
