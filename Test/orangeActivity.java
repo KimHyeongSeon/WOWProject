333be56000935e3e9653a3e65d81d69336fc813d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:45:37 +0900

    Update orangeActivity.java

--- Test/orangeActivity.java
+++ Test/orangeActivity.java
@@ -1,15 +1,12 @@
-package npp.a1234567;
-
-/**
- * Created by user on 2016-12-02.
- */
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class orangeActivity extends AppCompatActivity {
+public class orangeActivity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orange);
 
