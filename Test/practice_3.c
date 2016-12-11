7e92fe1bd2e13051851c51538d5c2cd25e26efe8
let0000 <wlsgh8309@naver.com>
2016-12-12 01:20:29 +0900

    Update practice_3.c

--- Test/practice_3.c
+++ Test/practice_3.c
@@ -5,4 +5,14 @@ int main(void)
   int arr[3] = { 10,20,30 };
   int *ptr = arr;
   
-  printf("%d %d %d\n", *ptr, *(ptr + 1), *(ptr + 2)); printf("%d\n", *ptr); ptr++; printf("%d\n", *ptr); ptr++; printf("%d\n", *ptr); ptr--; printf("%d\n", *ptr); ptr--; return 0;}
+  printf("%d %d %d\n", *ptr, *(ptr + 1), *(ptr + 2)); 
+  printf("%d\n", *ptr);
+  ptr++;
+  printf("%d\n", *ptr);
+  ptr++; 
+  printf("%d\n", *ptr);
+  ptr--; 
+  printf("%d\n", *ptr);
+  ptr--;
+  
+  return 0;}
