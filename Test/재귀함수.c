63e574f9835ca7aecdd721ee4c6454ddb5204871
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:24:32 +0900

    Update 재귀함수.c

--- Test/재귀함수.c
+++ Test/재귀함수.c
@@ -2,22 +2,23 @@
 
 void Recursive( int nCount )
 {
-if( nCount <= 0 )   
-{
-printf( "발사\n" );     
-return ;
-}
-else
-{ 
-printf( "카운트 다운 : %d\n", nCount ); 
+  if( nCount <= 0 )   
+  {
+    printf( "발사\n" );     
+    return ;
+  }
+  else
+  { 
+    printf( "카운트 다운 : %d\n", nCount ); 
 
-Recursive( nCount-1 );  
-//인자로 받은 nCount에 -1을 줄여서 호출한다.    
+    Recursive( nCount-1 );  
+    //인자로 받은 nCount에 -1을 줄여서 호출한다.    
+  }
 }
-}
+
 int main( )
 {
-Recursive( 5 );
-return 0;
+  Recursive( 5 );
+  return 0;
 }
 
