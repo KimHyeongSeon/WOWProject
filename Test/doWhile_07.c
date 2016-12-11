f185daba37615321442e6655de92f5ceea717666
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:18:51 +0900

    Update and rename doWhile07.c to doWhile_07.c

--- Test/doWhile_07.c
+++ Test/doWhile_07.c
@@ -6,21 +6,30 @@ void main()
 {
 
   int i = 1, j = 1, k = 1;
-    do{
-      do{
+    do
+    {
+      do
+      {
         k = 0;
-        do{
+        do
+        {
           if (i + k > 9)
+          {
             break;
+          }
           printf("%d * %d = %2d ", i + k, j, (i + k)*j);
           k++;
-        } while (k <= 4);
+        } 
+        while (k <= 4);
+        
         printf("\n");
         j++;
-      } while (j <= 9);
+      } 
+      while (j <= 9);
       printf("\n");
       i += k;
       j = 1;
-    } while (i <= 9);
+    } 
+    while (i <= 9);
     
 }
