116be4766953982d1b5b26d575982379374e8924
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:02:58 +0900

    Update function_03.c

--- Test/function_03.c
+++ Test/function_03.c
@@ -1,1 +1,40 @@
  
+#include "stdafx.h"
+#include "stdlib.h"
+
+int rectangle(int x)
+{
+	int i = 0;
+	int j = 0;
+	int num = 1;
+		
+	for (j = 0; j < x; j++)
+	{
+		for (i = 0; i < x; i++)
+		{
+			printf("%d ", num);
+			num++;
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+
+int main()
+{
+	int num = 0;
+
+	scanf("%d", &num);
+
+	rectangle(num);
+
+
+
+
+
+
+	system("pause");
+	return 0;
+}
