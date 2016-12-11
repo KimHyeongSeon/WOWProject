7e896a8ca67b065177c76532fbfff60ebeb8b5a1
let0000 <wlsgh8309@naver.com>
2016-12-12 00:35:30 +0900

    Update pointer_36.c

--- Test/pointer_36.c
+++ Test/pointer_36.c
@@ -10,4 +10,7 @@ int main(void)
   pi = (int*)10000;
   pd = (double*)10000;
   
-  printf("증가 전 pc=%d, pi=%d, pd=%d\n", pc, pi, pd); pc++; pi++; pd++; printf("증가 후 pc=%d, pi=%d, pd=%d\n", pc, pi, pd);  return 0;}
+  printf("증가 전 pc=%d, pi=%d, pd=%d\n", pc, pi, pd);
+  
+  pc++;
+  pi++; pd++; printf("증가 후 pc=%d, pi=%d, pd=%d\n", pc, pi, pd);  return 0;}
