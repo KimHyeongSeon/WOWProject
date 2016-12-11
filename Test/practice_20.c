f309dcdc0b271f1fdc29c9f63646e1bc4615210d
let0000 <wlsgh8309@naver.com>
2016-12-12 01:54:43 +0900

    Update practice_20.c

--- Test/practice_20.c
+++ Test/practice_20.c
@@ -7,4 +7,7 @@ int main(void)
   double *pd = (double*)100;
   
   printf("%u %u %u\n", pc - 1, pc, pc + 1);
-  printf("%u %u %u\n", pi - 1, pi, pi + 1); printf("%u %u %u\n", pd - 1, pd, pd + 1); return 0;}
+  printf("%u %u %u\n", pi - 1, pi, pi + 1);
+  printf("%u %u %u\n", pd - 1, pd, pd + 1); 
+  
+  return 0;}
