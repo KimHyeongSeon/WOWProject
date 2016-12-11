e4a34851afc37b6324ba8986fbf6a37ce00af946
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:35:53 +0900

    Update jungol_array_06.c

--- Test/jungol_array_06.c
+++ Test/jungol_array_06.c
@@ -1,1 +1,31 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int arr[10] = {};
+	int i = 0;
+	int min = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+
+	min = arr[0];
+
+	for (i = 0; i < 10; i++)
+	{
+		if (arr[i] <= min)
+		{
+			min = arr[i];
+		}
+	}
+	printf("%d", min);
+
+
+	system("pause");
+	return 0;
+}
