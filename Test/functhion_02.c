49fc0cd418762f55b181c4bc1f120bb3646887cb
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:01:19 +0900

    Update functhion_02.c

--- Test/functhion_02.c
+++ Test/functhion_02.c
@@ -1,1 +1,24 @@
  
+#include "stdafx.h"
+#include "stdlib.h"
+
+void area(int x)
+{
+	double PI = 3.14;
+
+	printf("%.2lf", PI * x * x);
+}
+
+int main()
+{
+	int radius = 0;
+
+	scanf("%d", &radius);
+
+	area(radius);
+
+
+	system("pause");
+	return 0;
+}
+
