e2e8dbcb4b7f36d597ca97d75cddb879c8823d53
let0000 <wlsgh8309@naver.com>
2016-12-12 01:49:06 +0900

    Update practice_16.c

--- Test/practice_16.c
+++ Test/practice_16.c
@@ -2,4 +2,8 @@
 
 int main()
 {
-  int j1=3, j2 =27; int * jint[2] = {&j1,&j2}; char * jchar[] = {"it's","jdpb"}; printf("%d%d\n",*jint[0],*jint[1]); printf("%s %s\n",jchar[0],jchar[1]); return 0;}
+  int j1=3, j2 =27;
+  int * jint[2] = {&j1,&j2};
+  char * jchar[] = {"it's","jdpb"};
+  
+  printf("%d%d\n",*jint[0],*jint[1]); printf("%s %s\n",jchar[0],jchar[1]); return 0;}
