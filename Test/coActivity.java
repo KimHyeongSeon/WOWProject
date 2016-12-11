4eca39acc705f1636a28b2a790291779cf829daa
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:37:26 +0900

    Create coActivity.java

---
+++ Test/coActivity.java
@@ -0,0 +1,18 @@
+package npp.a1234567;
+
+/**
+ * Created by user on 2016-12-02.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class coActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_co);
+
+    }
+
+}
