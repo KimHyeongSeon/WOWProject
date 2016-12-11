9097b9f7dec05b9106e379aa748bbe33c6831432
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:42:03 +0900

    Update if_23.c

--- Test/if_23.c
+++ Test/if_23.c
@@ -9,13 +9,17 @@ int _tmain(int argc, _TCHAR* argv[])
 	scanf_s("%d%d", &height,&weight);
 
 	if ((height - 100) * 1.1 <= weight)
+	{
 		printf("당신은 과체중입니다.\n");
-
+	}
 	else if ((height - 100) * 0.7 >=weight)
+	{
 		printf("당신은 저체중입니다.\n");
-
+	}
 	else
+	{
 		printf("당신은 표준체중입니다.\n");
+	}
 
 	system("pause");
 	return 0;
