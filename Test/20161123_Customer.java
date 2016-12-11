4131e7e310234653b71673f92ef28dd91b420ac4
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:02:13 +0900

    Create 20161123_Customer.java

---
+++ Test/20161123_Customer.java
@@ -0,0 +1,35 @@
+package kr.ac.sunmoon;
+
+public class Customer 
+{
+	private String gender;
+	private String name;
+	private String address;
+	
+	public String getGender() 
+	{
+		return gender;
+	}
+	public void setGender(String gender) 
+	{
+		this.gender = gender;
+	}
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public String getAddress() 
+	{
+		return address;
+	}
+	public void setAddress(String address) 
+	{
+		this.address = address;
+	}
+	
+	
+}
