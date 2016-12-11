ff67c2c0c4e0b5bea476c08408cef5356cb1a492
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:41:30 +0900

    Update bonActivity.java

--- Test/bonActivity.java
+++ Test/bonActivity.java
@@ -1,17 +1,14 @@
-package npp.a1234567;
-
-/**
- * Created by user on 2016-12-02.
- */
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class bonActivity extends AppCompatActivity {
+public class bonActivity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bon);
 
