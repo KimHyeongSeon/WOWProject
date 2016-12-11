9af1676891b9df35de545d2769ac4f8e39b83152
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:36:26 +0900

    Update if_22.c

--- Test/if_22.c
+++ Test/if_22.c
@@ -1,1 +1,22 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int temp;
+
+	printf("오늘의 기온을 입력해주세요!\n");
+	scanf_s("%d",&temp);
+
+	if (temp > 23)
+		printf("오늘의 기온은 %d도로 더운 날씨입니다.\n",temp);
+	else if (temp < 8)
+		printf("오늘의 기온은 %d도로 추운 날씨입니다.\n", temp);
+	else
+		printf("오늘의 기온은 %d도로 쾌적한 날씨입니다.\n", temp);
+
+	system("pause");
+	return 0;
+}
+
