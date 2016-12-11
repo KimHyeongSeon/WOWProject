60ee90b3fc46e942bf44d43ee87803c4d79d6fd3
let0000 <wlsgh8309@naver.com>
2016-12-12 01:29:10 +0900

    Update practice_9.c

--- Test/practice_9.c
+++ Test/practice_9.c
@@ -1,1 +1,7 @@
-#include "stdio.h"int add(int *arr3);int main(void){ int arr1[5] = { 1,2,3,4,5 }; arr1[0] = add(arr1); printf("%d", arr1[0]); return 0;}int add(int *arr3){ arr3[0] = 3; return arr3[0];}
+#include "stdio.h"
+
+int add(int *arr3);
+
+int main(void)
+{
+  int arr1[5] = { 1,2,3,4,5 }; arr1[0] = add(arr1); printf("%d", arr1[0]); return 0;}int add(int *arr3){ arr3[0] = 3; return arr3[0];}
