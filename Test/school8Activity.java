26ce556065079c65b0b13427cb29482636157f0a
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:50:50 +0900

    Update school8Activity.java

--- Test/school8Activity.java
+++ Test/school8Activity.java
@@ -1,14 +1,12 @@
-
-/**
- * Created by user on 2016-12-02.
- */
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class school8Activity extends AppCompatActivity {
+public class school8Activity extends AppCompatActivity 
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school8);
 
