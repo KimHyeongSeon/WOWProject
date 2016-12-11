ebfc25b462a309712af650d33e8cd76e7a8768f0
let0000 <wlsgh8309@naver.com>
2016-12-12 02:02:03 +0900

    Update practice_21.c

--- Test/practice_21.c
+++ Test/practice_21.c
@@ -1,1 +1,19 @@
-#include <stdio.h>int main(void){ int a[4] = {1, 3, 6, 8}; int *pa = &a[0]; printf("%d  %d  %d  %d\n", *(pa), *(pa+1), *(pa+2), *(pa+3)); printf("%d  %d  %d  %d\n", pa[0], pa[1], pa[2], pa[3]); printf("%d  ", ++*pa); printf("%d  ", *++pa); printf("%d  ", *pa++); printf("%d  ", --*pa++); printf("%d\n", (*pa)--); printf("%d  %d  %d  %d\n", a[0], a[1], a[2], a[3]); return 0;}
+#include <stdio.h>
+
+int main(void)
+{
+  int a[4] = {1, 3, 6, 8};
+  int *pa = &a[0];
+  
+  printf("%d  %d  %d  %d\n", *(pa), *(pa+1), *(pa+2), *(pa+3));
+  printf("%d  %d  %d  %d\n", pa[0], pa[1], pa[2], pa[3]); 
+  
+  printf("%d  ", ++*pa); 
+  printf("%d  ", *++pa);
+  printf("%d  ", *pa++);
+  printf("%d  ", --*pa++);
+  printf("%d\n", (*pa)--);
+  
+  printf("%d  %d  %d  %d\n", a[0], a[1], a[2], a[3]); 
+  
+  return 0;}
