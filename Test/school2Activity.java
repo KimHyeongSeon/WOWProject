04e8dbd0c2603a9be301905de53e7a1bd00c46e7
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 15:35:42 +0900

    Create school2Activity.java

---
+++ Test/school2Activity.java
@@ -0,0 +1,17 @@
+
+/**
+ * Created by user on 2016-12-02.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class school2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school2);
+
+    }
+
+}
