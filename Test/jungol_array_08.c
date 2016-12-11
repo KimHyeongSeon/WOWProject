0e6e45355ea85dcf3a0cfbb7fbc014277dbe434d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:36:40 +0900

    Update jungol_array_08.c

--- Test/jungol_array_08.c
+++ Test/jungol_array_08.c
@@ -1,1 +1,25 @@
 
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	int sumEven = 0; // 짝수
+	int sumOdd = 0; // 홀수
+
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	sumEven = arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+	sumOdd = arr[0] + arr[2] + arr[4] + arr[6] + arr[8];
+
+	printf("짝수번째 합 : %d\n", sumEven);
+	printf("홀수번째 평균 : %.1lf", (double)sumOdd / 5);
+
+	system("pause");
+	return 0;
+}
