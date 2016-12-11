53a0e62f4c4fca422e642b19fc9244e675cf0816
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 16:51:27 +0900

    Create jungol_input_03.c

---
+++ Test/jungol_input_03.c
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+int main()
+{
+	int x;
+	int y;
+
+	x = 55;
+	y = 10;
+	printf("%d - %d = %d\n", x, y, x-y);
+
+	x = 2008;
+	y = 1999;
+	printf("%d - %d = %d\n", x, y, x - y);
+
+
+	system("pause");
+	return 0;
+}
