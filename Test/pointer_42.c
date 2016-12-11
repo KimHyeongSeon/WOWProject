fd79044e4d496a9b43fa438ce8709454ea07fc6e
let0000 <wlsgh8309@naver.com>
2016-12-12 00:56:39 +0900

    Update pointer_42.c

--- Test/pointer_42.c
+++ Test/pointer_42.c
@@ -7,4 +7,9 @@ int main(int argc, const char * argv[])
     char *p;         
     p = &ch;         
     
-    printf("address ch = %p\n", &ch);        printf("address p = %p\n",p);            }        return 0;}
+    printf("address ch = %p\n", &ch);    
+    printf("address p = %p\n",p);    
+  }     
+  
+  return 0;}
+
