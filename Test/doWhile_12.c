4c89670743c188e37c884ce9e9034d0712066d30
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-11 17:27:41 +0900

    Create doWhile_12.c

---
+++ Test/doWhile_12.c
@@ -0,0 +1,22 @@
+
+#include "stdafx.h"
+#include "stdlib.h"
+
+void GetSumProduct(int a, int b, int sum, int product);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int i;
+
+	i = 1;
+	do
+	{
+		printf("%d ", i++);
+		printf("\n");
+	} while (i <= 100);
+
+
+
+	system("pause");
+	return 0;
+}
