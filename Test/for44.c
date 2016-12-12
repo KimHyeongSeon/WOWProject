6b9dbc75668dc355666c4148ea83de4190a67d30
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:48:08 +0900

    Update for44.c

--- Test/for44.c
+++ Test/for44.c
@@ -1,15 +1,23 @@
 
 #include <stdio.h>
+
 int main(void)
-{
+{   
   int d,f,i=1, j=0, k=0;
-  while(i<=100){
+  
+  while(i<=100)
+  {
     if(i%2 ==0)
+    {
       j +=i;
+    }
     else
+    {
       k +=i;
+    }
     i++;
   }
+  
   printf("짝수의 합 %d",j);
   printf("홀수의 합 %d",k);
 
