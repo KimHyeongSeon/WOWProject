daa2d8cc0e4f37631972201b127cc29968c8a8e6
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:46:02 +0900

    Update for11.c

--- Test/for11.c
+++ Test/for11.c
@@ -18,24 +18,3 @@ printf("홀수의 합 : %d", c);
 return 0;
 
 }
-
-
-while문 
-#include <stdio.h>
-int main(void){
-int d,f,i=1, j=0, k=0;
-while(i<=100){
-	if(i%2 ==0)
-		j +=i;
-	else
-		k +=i;
-	i++;
-}
-printf("짝수의 합 %d",j);
-printf("홀수의 합 %d",k);
-
-scanf("%d",&d);
-scanf("%d",&f);
-return 0;
-
-}
