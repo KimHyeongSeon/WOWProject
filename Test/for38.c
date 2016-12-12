f5592ab530e16bd98134071a6fc2714de9728460
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:42:34 +0900

    Update for38.c

--- Test/for38.c
+++ Test/for38.c
@@ -2,15 +2,14 @@
 #define ABS(x)(x>0 ? x : -x)
 #define AREA(w,h)(w*h/2)
 
-main()
+int main()
 {
-float a,b;	
-int c,e,g;
+	float a,b;	
+	int c,e,g;
 	
-	scanf("%f",&a);
+	scanf("%f",&a);	
 	b = ABS(a);
 	printf("%f\n",b);
-
 	scanf("%d",&c);
 	scanf("%d",&e);
 	g = AREA(c,e);
