c2c16aeb3a7fe5ca67410ea90d9a0dc8109b089d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:30:55 +0900

    Create for7.c

---
+++ Test/for7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define ABS(x)(x>0 ? x : -x)
+#define AREA(w,h)(w*h/2)
+
+main(){
+float a,b;	
+int c,e,g;
+	
+	scanf("%f",&a);
+	b = ABS(a);
+	printf("%f\n",b);
+
+	scanf("%d",&c);
+	scanf("%d",&e);
+	g = AREA(c,e);
+	printf("%d",g);
+
+	return 0;
+}
