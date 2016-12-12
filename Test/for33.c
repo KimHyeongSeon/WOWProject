ca69fc116db1d85b0658a38bf1e2db33bba02e7a
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:37:01 +0900

    Update for33.c

--- Test/for33.c
+++ Test/for33.c
@@ -2,18 +2,21 @@
 main()
 {
 	int n,d;
+	struct sungjuk
+	{
 		char a[10];
-		int e,f,g;};
-		struct sungjuk st, *ptr;
-		ptr=&st;
-		scanf("%s",ptr->a);
-		scanf("%d",&ptr->e);
-		scanf("%d",&ptr->f);
-		scanf("%d",&ptr->g);
+		int e,f,g;
+	};
+	struct sungjuk st, *ptr;
+	ptr=&st;
+	scanf("%s",ptr->a);
+	scanf("%d",&ptr->e);
+	scanf("%d",&ptr->f);
+	scanf("%d",&ptr->g);
 
-		n=ptr->e+ptr->f+ptr->g;
-		d=n/3;
+	n=ptr->e+ptr->f+ptr->g;
+	d=n/3;
 
-		printf("국어 : %d, 수학 : %d, 영어 : %d , 이름 : %s\n", ptr->e,ptr->f,ptr->g,ptr->a);
-		printf("총점 : %d , 평균 : %d",n,d);
+	printf("국어 : %d, 수학 : %d, 영어 : %d , 이름 : %s\n", ptr->e,ptr->f,ptr->g,ptr->a);
+	printf("총점 : %d , 평균 : %d",n,d);
 }
