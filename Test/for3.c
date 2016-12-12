15ac0ac160d744d46ed92ea6fe58088c37a53c6d
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:30:18 +0900

    Create for3.c

---
+++ Test/for3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void){
+struct sungjuk{
+	char f[10];
+	int a,b,c;
+};
+	struct sungjuk st;
+	int g,e;
+	scanf("%s",&(O,X?)st.f);
+	scanf("%d",&st.a);
+	scanf("%d",&st.b);
+	scanf("%d",&st.c);
+	
+	g=st.a+st.b+st.c;
+	e=g/3;
+
+	printf("이름 : %s\n국어 : %d\n수학 : %d\n영어 : %d\n총점 : %d\n평균 : %d", st.f,st.a,st.b,st.c,g,e);
+}
