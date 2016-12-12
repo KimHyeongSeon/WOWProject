c95375f84f275fe2682a3b1564a5e21a1343002b
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:45:07 +0900

    Update and rename for41.c to for42.c

---
+++ Test/for42.c
@@ -0,0 +1,16 @@
+
+	#include<stdio.h>
+	int main(void){
+	int b[4][3], i,j, c=1;
+
+	for(i=0;i<4;i++){
+		for(j=0;j<3;j++){
+			b[i][j]= c;
+		c++;}
+	}
+	for(i =0; i<3; i++){
+		for(j=0; j<i+1;j++){
+		printf("%d",b[i][j]);}
+		printf("\n");}
+	return 0;
+}
