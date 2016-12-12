3eec27c6f2935bef39b00e88b9bca0996f62fdac
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:32:38 +0900

    Update and rename for1.c to for32.c

--- Test/for32.c
+++ Test/for32.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
-main(void){
-	typedef struct sungjuk{
+main(void)
+{
+	typedef struct sungjuk
+	{
 		char f[10];
 		int a,b,c;}h;
 		h student[3];
 		int i,z,x;
-		for(i=0;i<=2;i++){
+		for(i=0;i<=2;i++)
+		{
 			printf("%d번째 학생이름\n", i+1);
 			scanf("%s", student[i].f);
 			printf("%d번째 국어점수\n", i+1);
@@ -20,4 +23,4 @@ main(void){
 			printf("이름 : %s , 국어 : %d , 수학 : %d , 영어 : %d\n", student[i].f,student[i].a,student[i].b,student[i].c);
 			printf("총점 : %d , 평균 : %d\n", z,x);
 		}
-		}
+}
