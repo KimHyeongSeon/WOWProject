08c4114cd7e82b27c6408a0bbb32e154455cc319
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:29:50 +0900

    Create for1.c

---
+++ Test/for1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(void){
+	typedef struct sungjuk{
+		char f[10];
+		int a,b,c;}h;
+		h student[3];
+		int i,z,x;
+		for(i=0;i<=2;i++){
+			printf("%d번째 학생이름\n", i+1);
+			scanf("%s", student[i].f);
+			printf("%d번째 국어점수\n", i+1);
+			scanf("%d", &student[i].a);
+			printf("%d번째 수학점수\n", i+1);
+			scanf("%d", &student[i].b);
+			printf("%d번째 영어점수\n", i+1);
+			scanf("%d", &student[i].c);
+			z=student[i].a+student[i].b+student[i].c;
+			x=z/3;
+			printf("이름 : %s , 국어 : %d , 수학 : %d , 영어 : %d\n", student[i].f,student[i].a,student[i].b,student[i].c);
+			printf("총점 : %d , 평균 : %d\n", z,x);
+		}
+		}
