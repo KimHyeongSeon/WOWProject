42bf58386204afb90816df698a3f1a835358f44e
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:43:44 +0900

    Update for40.c

--- Test/for40.c
+++ Test/for40.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-int main(void){
-	char a[]="Hello, C Programming!";
-int i;
-for(i=20; i>=0;i--){
 
-	printf("%c",a[i]);}
-return 0;
+int main(void)
+{
+	char a[]="Hello, C Programming!";
+	int i;
+	for(i=20; i>=0;i--)
+	{
+		printf("%c",a[i]);
+	}
+	return 0;
 }
