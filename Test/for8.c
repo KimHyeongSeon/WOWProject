fce574fbbbb3356ed6ae6755cfc91c1303058ab5
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:31:06 +0900

    Create for8.c

---
+++ Test/for8.c
@@ -0,0 +1,18 @@
+float min(float x, float y){
+	if(x>y)
+		return y;
+	else
+		return x;
+
+}
+
+#include<stdio.h>
+main(){
+float x, y, f;
+
+scanf("%f",&x);
+scanf("%f",&y);
+f = min(x,y);
+printf("%f",f);
+return 0;
+}
