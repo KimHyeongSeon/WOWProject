87128ce4ed1203807d2858e3ace386c6572bb4e7
yonghyun3221 <yonghyun3221@gmail.com>
2016-12-12 11:47:00 +0900

    Update for43.c

--- Test/for43.c
+++ Test/for43.c
@@ -1,18 +1,25 @@
 
 #include <stdio.h>
-int main(void){
-int a, b=0, c=0;
 
-	for(a=1 ; a<=100; a++){
+int main(void)
+{	
+	int a, b=0, c=0;
+
+	for(a=1 ; a<=100; a++)
+	{
 		if(a%2 ==0)
-		b += a;
-else
-	c += a;
-}
-printf("짝수의 합 : %d", b);
-printf("홀수의 합 : %d", c);
+		{
+			b += a;
+		}
+		else	
+		{
+			c += a;
+		}
+	}
+	printf("짝수의 합 : %d", b);
+	printf("홀수의 합 : %d", c);
 
 
-return 0;
+	return 0;
 
 }
